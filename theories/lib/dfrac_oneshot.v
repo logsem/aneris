ee41b6ede3ab6e4d00e7e19285545c50c2419550
Simon Gregersen <gregersen@cs.au.dk>
2021-05-03 12:59:47 +0200

    expose gname in dfrac_oneshot

--- theories/lib/dfrac_oneshot.v
+++ theories/lib/dfrac_oneshot.v
@@ -5,90 +5,76 @@ From iris.prelude Require Import options.
 
 Definition dfrac_oneshotR (A : ofe) := csumR dfracR (agreeR A).
 
-Class dfrac_oneshotG Σ A := {
-  dfrac_oneshotG_inG :> inG Σ (dfrac_oneshotR A);
-  dfrac_oneshotG_name : gname
-}.
-Class dfrac_oneshotGPreG Σ A := {
-  dfrac_oneshotG_inPreG :> inG Σ (dfrac_oneshotR A)
-}.
-
-Definition dfrac_oneshotGΣ A : gFunctors :=
-    #[GFunctor (csumR dfracR (agreeR A))].
-
-Global Instance subG_dfrac_oneshotG {Σ} (A : ofe) :
-  subG (dfrac_oneshotGΣ A) Σ → dfrac_oneshotGPreG Σ A.
-Proof. solve_inG. Qed.
-
 Section def.
-  Context `{dfrac_oneshotG Σ A}.
+  Context `{!inG Σ (dfrac_oneshotR A)}.
 
-  Definition pending_def (q : Qp) :=
-    own dfrac_oneshotG_name (Cinl (DfracOwn q)).
+  Definition pending_def (γ : gname) (q : Qp) :=
+    own γ (Cinl (DfracOwn q)).
   Definition pending_aux : seal (pending_def). by eexists. Qed.
   Definition pending := pending_aux.(unseal).
   Definition pending_eq : pending = pending_def := pending_aux.(seal_eq).
 
-  Definition pending_discarded_def :=
-    own dfrac_oneshotG_name (Cinl DfracDiscarded).
+  Definition pending_discarded_def (γ : gname) :=
+    own γ (Cinl DfracDiscarded).
   Definition pending_discarded_aux
     : seal pending_discarded_def. by eexists. Qed.
   Definition pending_discarded := pending_discarded_aux.(unseal).
   Definition pending_discarded_eq : pending_discarded = pending_discarded_def :=
     pending_discarded_aux.(seal_eq).
 
-  Definition shot_def a := own dfrac_oneshotG_name (Cinr (to_agree a)).
+  Definition shot_def γ a := own γ (Cinr (to_agree a)).
   Definition shot_aux : seal shot_def. by eexists. Qed.
   Definition shot := shot_aux.(unseal).
   Definition shot_eq : shot = shot_def := shot_aux.(seal_eq).
 End def.
 
-Global Arguments pending  {_ _ _} _%Qp.
+Global Arguments pending  {_ _ _} _ _%Qp.
 
-Lemma pending_alloc `{!dfrac_oneshotGPreG Σ A} :
-  ⊢ |==> ∃ _ : dfrac_oneshotG Σ A, pending 1.
+Lemma pending_alloc `{!inG Σ (dfrac_oneshotR A)} :
+  ⊢ |==> ∃ (γ : gname), pending γ 1.
 Proof.
   iIntros.
   iMod (own_alloc (Cinl (DfracOwn 1%Qp))) as (γ) "H".
   { rewrite //=. }
-  iExists {| dfrac_oneshotG_name := γ |}.
+  iExists _.
   rewrite pending_eq /pending_def. by iFrame.
 Qed.
 
 Section dfrac_oneshot_lemmas.
-  Context `{!dfrac_oneshotG Σ A}.
+  Context `{!inG Σ (dfrac_oneshotR A)}.
 
-  Global Instance pending_timeless q : Timeless (pending q).
+  Global Instance pending_timeless γ q : Timeless (pending γ q).
   Proof. rewrite pending_eq /pending_def. apply _. Qed.
 
-  Global Instance pending_discared_timeless : Timeless pending_discarded.
+  Global Instance pending_discared_timeless γ : Timeless (pending_discarded γ).
   Proof.
     rewrite /pending_discarded pending_discarded_aux.(seal_eq). apply _.
   Qed.
 
-  Global Instance pending_discared_persistent : Persistent pending_discarded.
+  Global Instance pending_discared_persistent γ :
+    Persistent (pending_discarded γ).
   Proof.
     rewrite /pending_discarded pending_discarded_aux.(seal_eq). apply _.
   Qed.
 
-  Global Instance shot_timeless `{OfeDiscrete A} a :
-    Timeless (shot a).
+  Global Instance shot_timeless `{OfeDiscrete A} γ a :
+    Timeless (shot γ a).
   Proof. rewrite shot_eq /shot_def. apply _. Qed.
 
-  Global Instance shot_persistent a : Persistent (shot a).
+  Global Instance shot_persistent γ a : Persistent (shot γ a).
   Proof. rewrite shot_eq /shot_def. apply _. Qed.
 
-  Lemma pending_split p1 p2 :
-    pending (p1 + p2) ⊣⊢ pending p1 ∗ pending p2.
+  Lemma pending_split γ p1 p2 :
+    pending γ (p1 + p2) ⊣⊢ pending γ p1 ∗ pending γ p2.
   Proof.
     rewrite pending_eq /pending_def -own_op -Cinl_op dfrac_op_own; auto.
   Qed.
 
   Definition nat_to_Qp n := pos_to_Qp (Pos.of_nat n).
 
-  Lemma big_sepS_pending_combine `{Countable B} (X : gset B) q :
-    ([∗ set] _ ∈ X, pending q) ⊣⊢
-    if decide (size X = 0) then True else pending ((nat_to_Qp (size X)) * q).
+  Lemma big_sepS_pending_combine `{Countable B} (X : gset B) γ q :
+    ([∗ set] _ ∈ X, pending γ q) ⊣⊢
+    if decide (size X = 0) then True else pending γ ((nat_to_Qp (size X)) * q).
   Proof.
     rewrite pending_eq /pending_def.
     induction X using set_ind_L.
@@ -103,9 +89,9 @@ Section dfrac_oneshot_lemmas.
       rewrite Pplus_one_succ_l -pos_to_Qp_add Qp_mul_add_distr_r Qp_mul_1_l //.
   Qed.
 
-  Lemma pending_split_gset `{Countable B} (X : gset B) :
+  Lemma pending_split_gset `{Countable B} (X : gset B) γ :
     X ≠ ∅ →
-    pending 1 ⊣⊢ [∗ set] _ ∈ X, pending (/ nat_to_Qp (size X)).
+    pending γ 1 ⊣⊢ [∗ set] _ ∈ X, pending γ (/ nat_to_Qp (size X)).
   Proof.
     intros Hnempty.
     rewrite big_sepS_pending_combine.
@@ -114,30 +100,31 @@ Section dfrac_oneshot_lemmas.
     rewrite Qp_mul_inv_r //.
   Qed.
 
-  Lemma pending_discard q :
-    pending q ==∗ pending_discarded.
+  Lemma pending_discard γ q :
+    pending γ q ==∗ pending_discarded γ.
   Proof.
     rewrite pending_eq /pending_def
             pending_discarded_eq /pending_discarded_def.
     apply own_update, csum_update_l, dfrac_discard_update.
   Qed.
 
-  Lemma pending_discarded_shot `{OfeDiscrete A} a :
-     pending_discarded -∗ shot a -∗ False.
+  Lemma pending_discarded_shot `{OfeDiscrete A} a γ :
+     pending_discarded γ -∗ shot γ a -∗ False.
   Proof.
     rewrite pending_discarded_eq /pending_discarded_def shot_eq /shot_def.
     iIntros "H H'".
     by iDestruct (own_valid_2 with "H H'") as %?.
   Qed.
 
-  Lemma pending_shot `{OfeDiscrete A} a q : pending q -∗ shot a  -∗ False.
+  Lemma pending_shot `{OfeDiscrete A} γ a q :
+    pending γ q -∗ shot γ a -∗ False.
   Proof.
     rewrite pending_eq /pending_def shot_eq /shot_def.
     iIntros "H H'".
     by iDestruct (own_valid_2 with "H H'") as %?.
   Qed.
 
-  Lemma pending_update_shot a : pending 1 ==∗ shot a.
+  Lemma pending_update_shot γ a : pending γ 1 ==∗ shot γ a.
   Proof.
     rewrite pending_eq /pending_def shot_eq /shot_def.
     iIntros "H". iMod (own_update with "H") as "$".
@@ -145,8 +132,8 @@ Section dfrac_oneshot_lemmas.
     done.
   Qed.
 
-  Lemma shot_agreeL `{!LeibnizEquiv A, !OfeDiscrete A} a b :
-    shot a -∗ shot b -∗ ⌜a = b⌝.
+  Lemma shot_agreeL `{!LeibnizEquiv A, !OfeDiscrete A} γ a b :
+    shot γ a -∗ shot γ b -∗ ⌜a = b⌝.
   Proof.
     rewrite shot_eq /shot_def.
     iIntros "H1 H2".
