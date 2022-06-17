5996a748666d55adf4cf3ddd802ba06630fefaab
Leon Gondelman <gondelman@cs.au.dk>
2022-06-17 16:17:25 +0200

    [lf] Proof of the client request handler at the follower.

---
+++ aneris/examples/reliable_communication/lib/repdb/proof/follower/proof_of_clients_handler.v
@@ -0,0 +1,130 @@
+From iris.algebra Require Import agree auth excl gmap dfrac.
+From iris.algebra.lib Require Import mono_list.
+From iris.base_logic Require Import invariants.
+From iris.bi.lib Require Import fractional.
+From iris.proofmode Require Import tactics coq_tactics reduction spec_patterns.
+From aneris.lib Require Import gen_heap_light.
+From aneris.aneris_lang Require Import lang resources inject tactics proofmode.
+From aneris.aneris_lang.lib Require Import
+     list_proof monitor_proof lock_proof map_proof.
+From aneris.aneris_lang.lib.serialization Require Import serialization_proof.
+From aneris.aneris_lang.program_logic Require Import aneris_lifting.
+From aneris.aneris_lang.program_logic Require Import aneris_weakestpre.
+From aneris.examples.reliable_communication.prelude
+     Require Import ser_inj.
+From aneris.examples.reliable_communication.lib.mt_server
+     Require Import user_params.
+From aneris.examples.reliable_communication.lib.repdb
+     Require Import repdb_code model.
+From aneris.examples.reliable_communication.lib.repdb.spec
+     Require Import db_params time events stdpp_utils.
+From aneris.examples.reliable_communication.lib.repdb.resources
+     Require Import ras log_resources resources_def
+     resources_global_inv resources_local_inv.
+From aneris.examples.reliable_communication.lib.repdb.proof
+     Require Import repdb_serialization log_proof.
+From aneris.examples.reliable_communication.lib.repdb.proof.follower
+     Require Import clients_at_follower_mt_user_params.
+
+Import gen_heap_light.
+Import lock_proof.
+
+Section Clients_MT_spec_params.
+
+  Context `{!anerisG Mdl Σ, !DB_params, !IDBG Σ}.
+  Context (γL γM γF: gname) (sa : socket_address).
+  Context (mγ : gname) (mv : val) (kvsL logL : loc).
+
+  Definition handler_cloj : val :=
+    λ: "mon" "req", client_request_handler_at_follower #kvsL "mon" "req".
+
+  Notation MTU := (client_handler_at_follower_user_params γL γM γF sa).
+
+  Lemma client_request_handler_at_follower_spec  :
+    ∀ reqv reqd,
+    {{{ follower_local_inv γL kvsL logL sa γF mγ mv ∗
+        lock_proof.locked mγ ∗
+        (log_monitor_inv_def
+           (ip_of_address MTU.(MTS_saddr)) γF (1/2) logL
+           (follower_local_res γL kvsL sa γF)) ∗
+           MTU.(MTS_handler_pre) reqv reqd }}}
+       handler_cloj mv reqv @[ip_of_address MTU.(MTS_saddr)]
+    {{{ repv repd, RET repv;
+        ⌜Serializable (rep_f2c_serialization) repv⌝ ∗
+        lock_proof.locked mγ ∗
+          (log_monitor_inv_def
+             (ip_of_address MTU.(MTS_saddr)) γF (1/2) logL
+             (follower_local_res γL kvsL sa γF)) ∗
+          MTU.(MTS_handler_post) repv reqd repd }}}.
+  Proof.
+    iIntros (reqv reqd Φ) "(#Hmon & Hkey & HR & Hpre) HΦ".
+    rewrite /handler_cloj /client_request_handler_at_follower. wp_pures.
+    iDestruct "HR" as (lV lM) "(%Hlog & Hpl & HlogL & HR)".
+    iDestruct "HR" as (kvsV kvsM) "(%Hkvs & %HvalidLocal & Hpm & #Hknw & #Hobs)".
+    iDestruct "Hpre" as "(#HGinv & HpreR)".
+    iDestruct "HpreR" as (k h Hkeys Hreqd ->) "#(Hknown & HobsL & Hobs2)".
+    wp_load.
+    iDestruct (own_obs_prefix _ _ _ h with "[$HlogL][$Hobs2]") as "%Hprefixh".
+    iDestruct (get_obs with "[$HlogL]") as "#HobsF".
+    inversion HvalidLocal.
+    wp_apply fupd_aneris_wp.
+    iAssert (|={⊤}=>
+               (⌜kvsM !! k = None⌝ ∗ ⌜at_key k lM = None⌝) ∨
+                 (∃ a : write_event, ⌜kvsM !! k = Some (we_val a)⌝ ∗
+                     ⌜at_key k lM = Some a⌝))%I
+      as ">Hpost".
+    { destruct (kvsM !! k) as [v|] eqn:Hmk. 
+      - iModIntro. iRight.
+        apply DB_LSTV_in_mem_log_some_coh_local in Hmk;
+          last by apply elem_of_dom.
+        destruct Hmk as (we0 & Hwe0L & <-).
+        iExists _. iSplit; first done. by iPureIntro. 
+      - iModIntro. iLeft. iSplit; first done. iPureIntro.
+        by apply DB_LSTV_in_mem_log_none_coh_local in Hmk. }
+    iModIntro.
+    wp_apply (wp_map_lookup $! Hkvs).
+    iIntros (v Hkm).
+    destruct (kvsM !! k) eqn:Hmk. 
+    - iDestruct "Hpost" as "[(%Habs & _)|Hpost]"; first done.
+      iDestruct "Hpost" as (a Ha) "%Hwe".
+      assert (v = SOMEV (we_val a)) as ->.
+      { rewrite Hmk in Hkm. naive_solver. }
+      iApply ("HΦ" $! (SOMEV (we_val a)) lM). iFrame "Hkey". iSplit.
+       { iPureIntro.  assert (k ∈ dom kvsM) as Hk by by apply elem_of_dom.
+         assert (v0 = (we_val a)) as Heqa by naive_solver.
+         rewrite Heqa in Hmk. 
+         rewrite Hmk in Hkm.
+         specialize (DB_LSTV_mem_serializable_vs_local k (we_val a) Hk Hmk).
+         apply _. }
+       simpl. rewrite /log_monitor_inv_def /ReqPost.
+       iSplitL; last first.
+       { iExists k, h, lM. do 3 (iSplit; first done). 
+         iFrame "#".
+         iRight. iExists a. eauto. } 
+       iExists _, _. iSplit; first done. iFrame "#∗". iExists _, _. by iFrame. 
+    - iApply ("HΦ" $! _ lM).
+      iDestruct "Hpost" as "[(_ & %Hnone) |%Habs]"; [|naive_solver].
+      assert (v = NONEV) as ->.
+      { rewrite Hmk in Hkm. naive_solver. }
+      iFrame "Hkey". iSplit.
+      { rewrite /rep_f2c_serialization. iPureIntro. apply _. }
+      simpl. rewrite /log_monitor_inv_def /ReqPost.
+      iSplitL; last first.
+      { iExists k, h, lM. do 3 (iSplit; first done). 
+        iFrame "#∗". iLeft. done. }
+      iExists _, _. iSplit; first done. iFrame "#∗". iExists _, _. by iFrame "#∗".
+  Qed.
+
+  Global Instance client_handler_at_leader_spec_params :
+    @MTS_spec_params _ _ _ _ MTU :=
+    {|
+      MTS_mR := (log_monitor_inv_def
+                   (ip_of_address MTU.(MTS_saddr)) γF (1/2) logL
+                   (follower_local_res γL kvsL sa γF));
+      MTS_mγ := mγ;
+      MTS_mv := mv;
+      MTS_handler := handler_cloj;
+      MTS_handler_spec := client_request_handler_at_follower_spec;
+    |}.
+
+End Clients_MT_spec_params.
