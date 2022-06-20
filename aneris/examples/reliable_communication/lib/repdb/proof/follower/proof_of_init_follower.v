d7c8e1fa29f3870aa602a1a17fe98eec797c3b5f
Leon Gondelman <gondelman@cs.au.dk>
2022-06-20 23:08:27 +0200

    [lf] The internal proof of the leader followers done (up to logical setup).

--- aneris/examples/reliable_communication/lib/repdb/proof/follower/proof_of_init_follower.v
+++ aneris/examples/reliable_communication/lib/repdb/proof/follower/proof_of_init_follower.v
@@ -30,6 +30,9 @@ From aneris.examples.reliable_communication.lib.repdb.proof
      Require Import
      log_proof
      repdb_serialization.
+From aneris.examples.reliable_communication.lib.repdb.proof.leader
+     Require Import
+     followers_mt_user_params.
 From aneris.examples.reliable_communication.lib.repdb.proof.follower
      Require Import
      clients_at_follower_mt_user_params
@@ -37,15 +40,20 @@ From aneris.examples.reliable_communication.lib.repdb.proof.follower
      proof_of_sync_loop.
 
 Section Init_Follower_Proof.
-  Context `{aG : !anerisG Mdl Σ, dbparams : !DB_params, dbg: !IDBG Σ}.
+  Context `{!anerisG Mdl Σ, !DB_params, !IDBG Σ}.
   Context (f2lsa f2csa : socket_address).
   Context (γL γM : gname) (N : gmap socket_address gname).
   Context (follower_si leaderF_si : message → iProp Σ).
   Context (InitFollower : iProp Σ).
   Notation MTC := (client_handler_at_follower_user_params γL γM N f2csa).
+  Notation MTF := (follower_handler_user_params γL γM N).
   Context (HInitFollowerSpec :
           ⊢  (∀ (MTS : MTS_spec_params MTC) A,
                 @run_server_spec _ _ _ _ _ InitFollower follower_si MTS A)).
+  Context (HinitFollowerAsClient :
+            ⊢ (∀ A sa,
+                @init_client_proxy_spec _ _ _ _ MTF leaderF_si A sa)).
+
   Context (γF : gname).
 
   Definition init_follower_res : iProp Σ :=
@@ -53,7 +61,8 @@ Section Init_Follower_Proof.
     own_log_auth γF (1/2) [] ∗
     own_logL_obs γL [] ∗
     InitFollower ∗
-    known_replog_token f2csa γF.
+    known_replog_token f2csa γF ∗
+    (∃ γdbF : gname, known_replog_token DB_addrF γdbF ∗ own_replog_obs γL DB_addrF []).
 
   Definition init_follower_spec_internal A : iProp Σ :=
         ⌜DB_addrF ∈ A⌝ →
@@ -88,7 +97,7 @@ Section Init_Follower_Proof.
     iIntros (logL logV) "(HpL & %HlogV)".
     wp_pures.
     iDestruct "HinitRes"
-      as "(#HGinv & HownF & #HobsL & Hinit & #HFtkn)".
+      as "(#HGinv & HownF & #HobsL & Hinit & #HFtkn & #HdbF)".
     iDestruct (get_obs with "[$HownF]") as "#HobsF".
     rewrite - {1} Qp_quarter_quarter.
     iDestruct (own_log_auth_split _ (1/4) (1/4) with "[$HownF]")
@@ -108,46 +117,67 @@ Section Init_Follower_Proof.
     { iPureIntro. apply valid_state_local_empty. }
     iFrame "#∗".
     iIntros (mγ mv) "#HLInv".
+    wp_let.
+    wp_bind (sync_with_server _ _ _ _ _ _).
+    rewrite /sync_with_server.
     wp_pures.
-    symmetry in HipEq.
-    rewrite {1} HipEq.
-    admit. 
-(*    wp_apply aneris_wp_fork.
-    iSplitR "HsrvInit Hmh Hfp".
+    rewrite {4} HipEq.
+    wp_apply (HinitFollowerAsClient $! A f2lsa with "[$HA $HmhF $HfpF $HsiF //]").
+    iIntros (reqh) "#HSpec".
+    wp_pures.
+    wp_apply aneris_wp_fork.
+    iSplitL "Hinit Hmh Hfp HΦ".
     - iNext.
       wp_pures.
-      wp_apply aneris_wp_fork.
-      iSplitR "HsrvFinit HmhF HfpF".
-      -- iNext.
-         wp_pures.
-         wp_apply aneris_wp_fork.
-         iSplitL "HΦ"; iNext; [ by iApply "HΦ"|].
-         rewrite -HipEq.
-         wp_apply (update_log_copy_loop_spec γL γM N _ γF mFγ
-                    with "[HownF2]"); [ | done].
-         iFrame "#∗".
-         rewrite /leader_local_secondary_inv.
-         rewrite /log_monitor_inv.
-         by rewrite HipEq.
-         Unshelve. done.
-      -- rewrite /start_leader_processing_followers.
-         iNext.
-         wp_pures.
-         assert (DB_addr ≠ DB_addrF) as Hneq.
-         { intro Heq. destruct DB_addr, DB_addrF. by inversion Heq. }
-         wp_apply
-           (HInitLeaderFSpec $! (follower_handler_spec_params
-                                   γL γM N mFγ γF mFv logLF Hneq) A _
-             with "[$HsrvFinit $HmhF $HfpF]");  eauto with iFrame.
-    - rewrite /start_leader_processing_clients.
-      iNext.
+      rewrite /start_follower_processing_clients.
       wp_pures.
-      rewrite -HipEq.
       wp_apply
-        (HInitLeaderSpec $! (client_handler_at_leader_spec_params
-                               γL γM N mγ mv kvsL logL) A _
-          with "[$HsrvInit $Hmh $Hfp]"); eauto with iFrame.
-  Qed. *)
-Admitted.
+        (HInitFollowerSpec $! (client_handler_at_leader_spec_params
+                                γL γM N f2csa mγ mv kvsL logL γF) A _
+                           with "[$Hinit $Hmh $Hfp][$HΦ]").
+      { iFrame "#". iSplit; first done. simplify_eq /=.
+        (* Of course, at least move this as a lemma into the monitor file.
+           Or better, improve later the definition of the followers_lock
+           to find optimal way to expose ghost names in the proofs.
+           By for now the low level proof below will do the job. *)
+        Local Transparent monitor_inv is_monitor.
+        rewrite /is_monitor /is_lock.
+        iDestruct "HLInv" as (lk ->) "HLInv".
+        iDestruct "HLInv" as (l ->) "HLInv".
+        iExists #l. iSplit; first done.
+        iExists l. iSplit; first done.
+        iApply (inv_iff with "[$HLInv]").
+        iNext. iModIntro.
+        rewrite /lock_inv.
+        iSplit.
+        - iIntros "(%b & (Hl & Hdef))".
+          iExists b. iFrame.
+          destruct b; first done.
+          iDestruct "Hdef" as "(Hk & Hdef)".
+          iSplitL "Hk"; first done.
+          iSplit; done.
+        - iIntros "(%b & (Hl & Hdef))".
+          iExists b. iFrame.
+          destruct b; first done.
+          iDestruct "Hdef" as "(Hk & _ & Hdef)".
+          by iFrame. }
+    - iNext.
+      rewrite -HipEq.
+      wp_apply (sync_loop_spec γL γM N f2csa kvsL logL mγ mv reqh [] 0
+                 with "[HownF2]"); [naive_solver| | done].
+      iFrame "HGinv".
+      iSplitR; last first.
+      { iSplitR. { iExists f2lsa. eauto. }
+        iFrame "#"; eauto.
+        iExists _; iFrame "#∗".
+        iDestruct "HdbF" as (γdbF) "#(Htk & HobsdbF)".
+        iDestruct "HobsdbF" as (γdbF') "(Htk' & _ & HobsdF)".
+        iDestruct (known_replog_token_agree with "[$Htk][$Htk']") as "->".
+        eauto. }
+      rewrite /follower_local_inv.
+      iExists γF.
+      iFrame "#".
+  Qed.
+
 
 End Init_Follower_Proof.
