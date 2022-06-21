aca285aee451c2e5c27c3a0e6aa0ad30b28da23b
Leon Gondelman <gondelman@cs.au.dk>
2022-06-21 08:43:30 +0200

    [lf] more proof for init_setup.

--- aneris/examples/reliable_communication/lib/repdb/proof/proof_of_db_init.v
+++ aneris/examples/reliable_communication/lib/repdb/proof/proof_of_db_init.v
@@ -1,4 +1,9 @@
 From iris.algebra Require Import excl.
+From iris.algebra Require Import auth gmap dfrac.
+From iris.algebra.lib Require Import mono_list.
+From iris.bi.lib Require Import fractional.
+From aneris.prelude Require Import collect.
+From aneris.lib Require Import gen_heap_light.
 From aneris.aneris_lang Require Import ast.
 From aneris.aneris_lang.lib.serialization Require Import serialization_code.
 From aneris.aneris_lang Require Import lang.
@@ -17,7 +22,17 @@ From aneris.examples.reliable_communication.lib.dlm
      Require Import dlm_prelude dlm_resources dlm_code dlm_spec.
 From aneris.examples.reliable_communication.lib.repdb.spec
      Require Import
-     ras events resources api_spec.
+     ras
+     events
+     resources
+     api_spec.
+From aneris.examples.reliable_communication.lib.repdb.resources
+     Require Import
+     ras
+     log_resources
+     resources_def
+     resources_local_inv
+     resources_global_inv.
 From aneris.examples.reliable_communication.lib.repdb.proof
      Require Import
      log_proof
@@ -40,6 +55,42 @@ From aneris.examples.reliable_communication.lib.repdb.proof.follower
      proof_of_sync_loop
      proof_of_init_follower.
 
+Section Alloc_ghost_resources.
+  Context `{!anerisG Mdl Σ, DB : !DB_params, !IDBG Σ }.
+
+  (* Move later to resources/?.v *)
+
+  Lemma alloc_gmem :
+    ⊢ |==>
+          ∃ γM : gname,
+            own_mem_sys γM (gset_to_gmap (@None write_event) DB_keys) ∗
+           ([∗ set] k ∈ DB_keys, own_mem_user γM k 1%Qp None).
+  Proof.
+    (* B induction on the set DB_keys. *)
+    (* set (empty_mem := (gset_to_gmap (@None write_event) DB_keys)). *)
+    (* iMod (gen_heap_light_init empty_mem) as "(%γM & Hmem)". *)
+  Admitted.
+
+  Lemma alloc_leader_logM  :
+   ⊢ |==>∃ γL, own_obs γL DB_addr [] ∗ own_log_auth γL 1 [].
+  Proof.
+  Admitted.
+
+  Lemma alloc_logM_and_followers_gnames :
+    DB_addrF ∉ DB_followers →
+    known_replog_tokens ∅ ⊢ |==>
+          ∃ (N : gmap socket_address gname),
+            ⌜dom N = DB_followers ∪ {[DB_addrF]}⌝ ∗
+            known_replog_tokens N ∗
+            ([∗ map] sa ↦ γ ∈ N,
+               known_replog_token sa γ ∗
+               own_log_obs γ [] ∗
+               own_log_auth γ 1 []).
+  Proof.
+    (* By induction on the set DB_followers ∪ DB_addrF. *)
+  Admitted.
+
+End Alloc_ghost_resources.
 
 Section Init_setup_proof.
   Context `{!anerisG Mdl Σ, DB : !DB_params, !DBPreG Σ }.
@@ -68,15 +119,25 @@ Section Init_setup_proof.
                                           Init_follower f_si leaderF_si) ∗
            (∀ A f2csa csa, init_client_proxy_follower_spec A csa f2csa f_si)).
   Proof.
-  (* iMod (own_alloc (● (∅ : gmapUR socket_address (agreeR gnameO)))) as (γknwF) "Hknw"; *)
-  (*   first by apply auth_auth_valid. *)
-  (* iMod (own_alloc (● (GSet ∅ : (gset_disjUR socket_address)))) as (γfreF) "Hfre"; *)
-  (*   first by apply auth_auth_valid. *)
-  (* set (db := *)
-  (*        {| *)
-  (*          DBG_known_replog_name := γknwF; *)
-  (*          DBG_free_replog_set_name := γfreF *)
-  (*        |}). *)
+    iIntros (HE Hn1 Hn2).
+    iMod (own_alloc
+            (● (to_agree <$> ∅ : (gmapUR socket_address (agreeR gnameO))))) as
+      (γFls) "Hgnames"; first by apply auth_auth_valid.
+    set ( dbg :=
+            {|
+              IDBG_Global_mem := DB_preG_Global_mem;
+              IDBG_Global_history_mono := DB_preG_Global_history_mono;
+              IDBG_Known_replog := DB_preG_Known_replog;
+              IDBG_lockG := DB_preG_lockG;
+              IDBG_known_replog_name := γFls
+            |}).
+    iMod (alloc_gmem) as (γM) "(HownSys & HownUser)".
+    iMod (alloc_leader_logM) as (γL) "(#HobsL & HlogLM)".
+    iMod (alloc_logM_and_followers_gnames with "[$Hgnames]")
+      as (N) "(%Hdom & Hreplog & Hmap)"; first done.
+    set (DBR := DbRes γL γM N).
+    iExists DBR.
+    (* Next step : Use MTS_init. *)
   Admitted.
 
   Global Instance db_init_instance : DB_init.
