0ff1accccdcbe803c9c5516231e72fa52a2812c8
Leon Gondelman <gondelman@cs.au.dk>
2023-06-04 13:12:04 +0200

    [SI] todo example 1

---
+++ aneris/examples/snapshot_isolation/examples/example_1_code.v
@@ -0,0 +1,12 @@
+(** TODO: This file shall be generated automatically from the OCaml sources
+    once implemented. *)
+
+From aneris.aneris_lang Require Import ast.
+From aneris.aneris_lang.lib Require Import map_code.
+From aneris.aneris_lang.lib Require Import network_util_code.
+From aneris.aneris_lang.lib.serialization Require Import serialization_code.
+From aneris.examples.reliable_communication.lib.repdb Require Import log_code.
+From aneris.examples.reliable_communication.lib.mt_server Require Import mt_server_code.
+
+(** FIXME: TODO *)
+(* TODO : IMPLEMENT THOSE IN OCAML AND TRANSLATE TO COQ *)
