a871a13b23c558b205972b992a29d3713c580b68
Léon <leon.ducruet@ens-lyon.fr>
2023-06-14 14:08:38 +0200

    Implementation folder containing some useful instances created

--- aneris/examples/snapshot_isolation/examples/example2/example2_proof.v
+++ aneris/examples/snapshot_isolation/examples/example2/example2_proof.v
@@ -1,15 +1,89 @@
-From aneris.aneris_lang Require Import resources proofmode.
-From aneris.examples.snapshot_isolation.specs Require Import specs.
+From aneris.aneris_lang Require Import tactics adequacy proofmode.
 From aneris.examples.snapshot_isolation.examples.example2
         Require Import example2_code.
 From aneris.examples.snapshot_isolation.specs
-     Require Import user_params time events resources.
+     Require Import user_params time events resources specs.
 From aneris.examples.snapshot_isolation
      Require Import snapshot_isolation_code_api.
+From iris.base_logic.lib Require Import invariants.
+From aneris.aneris_lang.lib.serialization Require Import serialization_proof.
 
 Section proof_of_code.
 
-  Context `{!anerisG Mdl Σ, !User_params, !KVS_time, !KVSG Σ,
+  Context (addr : socket_address).
+
+  Local Instance example2_params : User_params :=
+    {|
+      KVS_address := addr;
+      KVS_keys := {["x"; "y"]};
+      KVS_InvName := nroot .@ "kvs_inv";
+      KVS_serialization := int_serialization;
+    |}.
+
+  Context `{!anerisG Mdl Σ, !KVS_time, !KVSG Σ,
             !KVS_snapshot_isolation_api, !SI_resources Mdl Σ}.
+
+  Definition local_inv_def : iProp Σ :=
+    ("x" ↦ₖ None ∗ "y" ↦ₖ None) ∨
+    (∃ w1 w2, "x" ↦ₖ Some w1 ∗ "y" ↦ₖ Some w2 ∗
+              ⌜we_val w1 = #1⌝ ∗ ⌜we_val w2 = #1⌝).
+
+  Definition N := nroot .@ "example".
+
+  Definition local_inv := inv N local_inv_def.
+
+  Lemma transaction1_spec cst sa :
+    {{{
+      local_inv ∗
+      ConnectionState cst CanStart ∗
+      start_spec ∗
+      write_spec ∗
+      commit_spec
+    }}}
+      transaction1 cst @[ip_of_address sa]
+    {{{ RET #(); True }}}.
+  Proof.
+    iIntros (Φ) "(#inv & CanStart & #start & #write & #commit) HΦ".
+    rewrite/transaction1.
+    wp_pures.
+    
+  Admitted.
+
+  Lemma transaction2_spec cst sa :
+    {{{
+      local_inv ∗
+      ConnectionState cst CanStart
+    }}}
+      transaction2 cst @[ip_of_address sa]
+    {{{ RET #(); True }}}.
+  Proof.
+  Admitted.
+
+  Lemma transaction1_client_spec sa :
+    {{{
+      unallocated {[sa]} ∗
+      KVS_address ⤇ KVS_si ∗
+      sa ⤳ (∅, ∅) ∗
+      free_ports (ip_of_address sa) {[port_of_address sa]}
+    }}}
+      transaction1_client #sa #KVS_address @[ip_of_address sa]
+    {{{ RET #(); True }}}.
+  Proof.
+  Admitted.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 End proof_of_code.
---
+++ aneris/examples/snapshot_isolation/instantiation/snapshot_isolation_api_implementation.v
@@ -0,0 +1,33 @@
+From aneris.examples.snapshot_isolation
+     Require Import snapshot_isolation_code.
+From aneris.examples.snapshot_isolation.specs
+     Require Import user_params time events resources specs.
+From aneris.examples.snapshot_isolation
+     Require Import snapshot_isolation_code_api.
+From aneris.examples.reliable_communication.prelude Require Import ser_inj.
+
+Section implementation.
+
+  Global Instance KVS_snapshot_isolation_api_implementation :
+          KVS_snapshot_isolation_api :=
+    {|
+      SI_init_server := init_server;
+      SI_start := start;
+      SI_read := read;
+      SI_write := write;
+      SI_commit := commit;
+      SI_run := run;
+      SI_init_client_proxy := init_client_proxy;
+    |}.
+
+  Context `{!anerisG Mdl Σ, !User_params, !KVS_time}.
+
+  Context (KVS_ser_inj : ser_is_injective KVS_serialization).
+  Context (KVS_ser_inj_alt : ser_is_injective_alt KVS_serialization).
+
+  Global Instance SI_implementation_init : SI_init.
+  Proof.
+    (* TODO *)
+  Admitted.
+
+End implementation.
