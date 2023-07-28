036b6287a0204a919a7f298d86fd46a5f059050a
Leon Gondelman <gondelman@cs.au.dk>
2023-07-28 13:55:50 +0200

    [SI] wip on proof of read

--- aneris/examples/snapshot_isolation/proof/client_proxy/proof_of_read.v
+++ aneris/examples/snapshot_isolation/proof/client_proxy/proof_of_read.v
@@ -38,17 +38,68 @@ Section Read_Proof.
 
  Definition read_spec_internal {MTR : MTS_resources} : iProp Σ :=
     ∀ (c : val) (sa : socket_address)
-      (k : Key) (vo : option val) E,
+      (k : Key) (vo : option val),
     ⌜k ∈ KVS_keys⌝ -∗
+    @make_request_spec _ _ _ _ MTC _ -∗
     {{{ is_connected γGsnap γT γKnownClients c sa ∗
         ownCacheUser γKnownClients k c vo }}}
-      SI_read c #k @[ip_of_address sa] E
+      SI_read c #k @[ip_of_address sa]
     {{{ RET $vo; ownCacheUser γKnownClients k c vo }}}.
 
 
   Lemma read_spec_internal_holds {MTR : MTS_resources}  :
-     Global_Inv clients γKnownClients γGauth γGsnap γT ⊢ read_spec_internal.
+    ⊢ read_spec_internal.
   Proof.
+    iIntros (c sa k vo Hk) "#Hspec !#".
+    iIntros (Φ) "(#Hisc & Hcache) Hpost".
+    iDestruct "Hisc" as (lk cst l sv s) "Hisc".
+    iDestruct "Hisc" as (γCst γlk γS γA γCache ->) "#(Hc1 & Hisc)".
+    rewrite /SI_read /= /read.
+    wp_pures.
+    wp_apply (acquire_spec (KVS_InvName.@socket_address_to_str sa)
+               with "[$Hisc]").
+    iIntros (uu) "(_ & Hlk & Hres)".
+    wp_pures.
+    iDestruct "Hres"
+      as "(Hl & Hcr & [( -> & -> & Hres_abs & Htk) | Hres])".
+    { iDestruct "Hcache" as (? ? ? ? ? ? ? ? Heq) "Hcache".
+      symmetry in Heq. simplify_eq.
+      iDestruct "Hcache" as "(#Hc2 & Helem & %Hval)".
+      iDestruct (client_connected_agree γGsnap γT
+                  with "[$Hc2][$Hc1]") as "%Heq'".
+      simplify_eq /=.
+      by iDestruct (ghost_map.ghost_map_lookup
+                  with "[$Hres_abs][$Helem]")
+                  as "%Habs". }
+    iDestruct "Hres"
+      as (ts Msnap cuL cuV cuM cM -> -> Hcoh Hvalid)
+           "(%Hm & #Hts & #Hsn & HcM & Hauth & Htk)".
+    wp_load.
+    wp_pures.
+    wp_load.
+    wp_apply (wp_map_lookup $! Hm).
+    admit.
+    (* iIntros (v1 Hv1). *)
+(*     destruct (cuM !! k) eqn:Hkv1 *)
+(*     destruct vo eqn:Hvo. *)
+
+(*     - simplify_eq /=. *)
+(*       wp_pures. *)
+(*       wp_apply (release_spec with *)
+(*                "[$Hisc $Hlk Hl Hcr HcM Hauth Htk] [Hcache Hpost]"). *)
+(*     { iFrame "#∗". *)
+(*       iRight. *)
+(*       iExists ts, Msnap, cuL, _, cuM, cM. *)
+(*       iFrame "#∗". *)
+(*       iPureIntro. *)
+(*       split_and!; eauto. } *)
+(*     iNext. *)
+(*     iIntros (v1 ->). *)
+(*     wp_pures.  *)
+(*     destruct v eqn:Hv. *)
+(*       -- simplify_eq /=. *)
+(*          iApply ("Hpost" with "[Hcache]").       *)
+(* } *)
   Admitted.
 
 End Read_Proof.
