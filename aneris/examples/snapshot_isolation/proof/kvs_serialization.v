5604efa77725076a9d02b6a98bb6f02bf29455b4
Leon Gondelman <gondelman@cs.au.dk>
2023-08-23 08:43:50 +0200

    [SI] serialization of lists recovered

--- aneris/examples/snapshot_isolation/proof/kvs_serialization.v
+++ aneris/examples/snapshot_isolation/proof/kvs_serialization.v
@@ -1,6 +1,7 @@
+From aneris.prelude Require Import misc.
 From aneris.aneris_lang Require Import lang.
 From aneris.aneris_lang Require Import proofmode.
-
+From aneris.aneris_lang.lib Require Import inject list_proof network_util_proof.
 From aneris.aneris_lang.lib.serialization Require Import serialization_proof.
 From aneris.examples.reliable_communication.prelude Require Import ser_inj.
 From aneris.examples.snapshot_isolation.specs
@@ -9,47 +10,155 @@ From aneris.examples.snapshot_isolation.specs
 (** TODO: proof list_serialization and move to the serialization_proof file. *)
 Section list_serialization.
 
-  Context (A : serialization).
+  Context (E : serialization).
+  Context `{!Inject A val}.
 
-  Definition list_valid_val (v : val) := True.
+  Definition list_valid_val (v : val) :=
+    ∃ (la: list A), is_list la v ∧ (∀ x, x ∈ la → s_valid_val E $ x).
 
-  Definition list_ser_str (s1 s2 : string) := True.
+  Fixpoint list_is_ser_aux (la : list A) (s : string) :=
+    match la with
+    | hd :: tl =>
+          ∃ s1 s2 : string,
+            E.(s_is_ser) $hd s1 ∧
+            s = prod_ser_str s1 s2 ∧
+            list_is_ser_aux tl s2
+      | [] => s = ""
+  end.
 
-  Definition list_is_ser (v : val) (s : string) := True.
+  Definition list_is_ser (v : val) (s : string) :=
+    ∃ (l : list A), is_list l v ∧ list_is_ser_aux l s.
 
-  Lemma list_is_ser_valid v s : list_is_ser v s -> list_valid_val v.
+  Lemma list_is_ser_aux_valid_val l :
+    ∀ s x, list_is_ser_aux l s → x ∈ l → s_valid_val E $ x.
   Proof.
-  Admitted.
+    induction l as [|a' l']; first by set_solver.
+    intros ?? (?&?&?&?&?) [->|Hin]%elem_of_cons;
+      by [eapply s_is_ser_valid| eapply IHl'].
+  Qed.
+
+  Lemma list_is_ser_valid (v : val) (s : string) :
+    list_is_ser v s -> list_valid_val v.
+  Proof.
+    destruct 1 as (l&Hl&Hs).
+    exists l. split; first done.
+    intros x Hx. by eapply list_is_ser_aux_valid_val.
+  Qed.
 
   Lemma list_ser_spec `{!anerisG Mdl Σ} ip v:
     {{{ ⌜list_valid_val v⌝ }}}
-      list_ser A.(s_serializer).(s_ser) v @[ip]
+      (list_serializer (s_serializer E)).(s_ser) v @[ip]
     {{{ (s : string), RET #s; ⌜list_is_ser v s⌝ }}}.
   Proof.
-  Admitted.
+    iIntros (Φ) "Hv HΦ".
+    iLöb as "IH" forall (Φ v).
+    wp_rec.
+    iDestruct "Hv" as %(l&Hvl&Hvv).
+    destruct l as [|a l].
+    - rewrite Hvl.
+      wp_pures.
+      iApply "HΦ".
+      iPureIntro.
+      rewrite /list_is_ser; eexists []; done.
+    - simpl in Hvl, Hvv.
+      destruct Hvl as [lv [-> Hvl]].
+      wp_pures.
+      wp_apply (s_ser_spec E).
+      { iPureIntro.
+        apply Hvv.
+        set_solver. }
+      iIntros (s1) "%Hs1".
+      wp_pures.
+      wp_bind (list_ser (s_ser (s_serializer E)) _).
+      iApply "IH"; [iPureIntro; exists l; set_solver |].
+      iIntros "!>" (s2) "%Hs2".
+      wp_pures.
+      destruct Hs2 as (l'& Hs2x & Hs2y).
+      iApply "HΦ".
+      iPureIntro.
+      exists (a :: l').
+      split; first by eexists.
+      by exists s1, s2.
+  Qed.
 
   Lemma list_deser_spec `{!anerisG Mdl Σ} ip v s:
     {{{ ⌜list_is_ser v s⌝ }}}
-      list_deser A.(s_serializer).(s_deser) #s @[ip]
-    {{{ RET v; True }}}.
+      (list_serializer (s_serializer E)).(s_deser) #s @[ip]
+      {{{ RET v; True }}}.
   Proof.
-  Admitted.
+    iIntros (Φ) "Hs HΦ".
+    iLöb as "IH" forall (Φ v s).
+    wp_rec.
+    iDestruct "Hs" as %(l & Hl1 & Hl2).
+    destruct l as [|a l]; simpl.
+    - rewrite Hl1 Hl2.
+      wp_find_from; first by split_and!; [|by apply nat_Z_eq; first lia].
+      wp_pures.
+      by iApply "HΦ".
+    - destruct Hl1 as [lv [-> Hl1]].
+      destruct Hl2 as (s1&s2&Hs1&->&Hl2).
+      rewrite! /prod_ser_str.
+      wp_find_from; first by split_and!; [|by apply nat_Z_eq; first lia].
+      erewrite (index_0_append_char ); auto; last first.
+      { apply valid_tag_stringOfZ. }
+      wp_pures.
+      wp_substring; first by split_and!; [|by apply nat_Z_eq; first lia|done].
+      rewrite substring_0_length_append.
+      wp_pure _.
+      { simpl. rewrite ZOfString_inv //. }
+      wp_apply wp_unSOME; [done|].
+      iIntros "_ /=". wp_pures.
+      wp_substring;
+        first by split_and!;
+              [|by apply nat_Z_eq; first lia|by apply nat_Z_eq; first lia].
+      replace (Z.to_nat (Z.add (Z.of_nat
+                                  (String.length
+                                     (StringOfZ (Z.of_nat (String.length s1)))))
+                               1%Z)) with
+        (String.length (StringOfZ (Z.of_nat (String.length s1))) + 1)%nat by lia.
+      replace (Z.to_nat (String.length s1)) with (String.length s1)%nat by lia.
+      rewrite substring_add_length_app /= substring_0_length_append.
+      wp_pures.
+      rewrite !length_app /=.
+      match goal with
+      | |- context [Substring _ _ ?X] =>
+          replace X with (Val #(String.length s2)); last first
+      end.
+    { repeat f_equal; lia. }
+    wp_substring; first by split_and!; [|by apply nat_Z_eq; first lia|done].
+    match goal with
+    | |- context [substring ?X _ _] =>
+      replace X with (String.length
+                        (StringOfZ (Z.of_nat (String.length s1))) + 1 +
+                        String.length s1)%nat by lia
+    end.
+    rewrite -Nat.add_assoc substring_add_length_app /= substring_length_append.
+    wp_pures.
+    wp_apply (s_deser_spec E); first done.
+    iIntros "_"; simpl.
+    wp_pures.
+    wp_bind (list_deser _ _).
+    iApply ("IH" $! _ lv); first by iPureIntro; eexists.
+    iIntros "!> _".
+    wp_pures.
+    wp_apply (wp_list_cons _ l); first done.
+    iIntros (v Hl).
+    assert ((InjRV ($ a, lv) = v)) as ->.
+    { destruct Hl as [lv' [-> Hl1']].
+      by rewrite (is_list_eq _ _ _ Hl1 Hl1'). }
+    by iApply "HΦ".
+  Qed.
 
-  Definition list_serialization : serialization :=
-    {| s_valid_val := list_valid_val;
-       s_serializer := list_serializer A.(s_serializer);
-       s_is_ser := list_is_ser;
-       s_is_ser_valid := list_is_ser_valid;
-       s_ser_spec := @list_ser_spec;
-       s_deser_spec := @list_deser_spec; |}.
-
-  Global Instance:
-    ∀ v1, Serializable A v1 →
-             Serializable list_serialization v1.
-  Proof. rewrite /Serializable /= /list_valid_val /=; eauto. Qed.
+Definition list_serialization : serialization :=
+  {| s_valid_val := list_valid_val;
+     s_serializer := list_serializer E.(s_serializer);
+     s_is_ser := list_is_ser;
+     s_is_ser_valid := list_is_ser_valid;
+     s_ser_spec := @list_ser_spec;
+     s_deser_spec := @list_deser_spec; |}.
 
   Global Instance list_serializer_of serA :
-    SerializerOf serA A →
+    SerializerOf serA E →
     SerializerOf (list_serializer serA) (list_serialization).
   Proof.
     intros H1.
