8c2568f20c04942248f2d8ef48acedb50c903453
Léon Gondelman <leon.gondelman@gmail.com>
2023-09-22 13:56:47 +0200

    [SI] wip on read proof.

--- aneris/examples/snapshot_isolation/proof/server/proof_of_read_handler.v
+++ aneris/examples/snapshot_isolation/proof/server/proof_of_read_handler.v
@@ -133,15 +133,18 @@ Section Proof_of_read_handler.
     }}}.
   Proof.
     iIntros (Het Hm Hts Hcoh HcohM Hin Hpre Φ) "_ HΦ".
-    wp_lam. do 15 wp_pures.
-    destruct Hcoh.
+    wp_lam. do 15 wp_pures. destruct Hcoh.
+    (** We start by getting the versions at the key k. *)
     wp_smart_apply (kvs_get_spec k kvsV Hk m M Hm Hin); eauto.
     iIntros (v) "%Hvlo".
     clear m Hm kvsl_ValidDom kvsl_ValidInModelEmpty
       kvsl_ValidInModelSome kvsl_ValidLocalSome
       kvsl_ValidModel Tss.
-    iLöb as "IH" forall (v M Hk Hin Hpre Hvlo HcohM).
+    (** Now we can proceed by induction on the list of versions Hk. *)
+    (iLöb as "IH" forall (v M Hk Hin Hpre Hvlo HcohM)).
     destruct (reverse Hk) as [| e Hr] eqn:Hkeq.
+    (** There are two cases to consider. 
+        First, we assume that the history is empty.  *)
     - subst; simpl.
       wp_pures.
       iApply ("HΦ" $! None); eauto.
@@ -149,6 +152,9 @@ Section Proof_of_read_handler.
       rewrite -reverse_nil in Hkeq.
       list_simplifier.
       by apply prefix_nil_inv in Hpre.
+    (* Suppose now that there exists e and Hr such that 
+       reverse Hk = e :: Hr. We start by rewriting Hr so that
+       we can later apply the induction hypothesis. *)
     - assert (∃ Hr', Hr = reverse Hr') as (Hr' & Hrr).
       { subst. destruct (reverse Hk); first done. exists (reverse (Hr)).
         by rewrite reverse_involutive. }
@@ -156,53 +162,86 @@ Section Proof_of_read_handler.
       rewrite Hrr in Hvlo.
       clear Hrr Hr.
       rewrite Hvlo.
+      (** We now go throught the body of the function. *)
       wp_pures.
+      (** First we prove that `ts` is indeed a start time,
+       so no element `e` in the memory has it as a commit time. *)
       case_bool_decide.
-      (** should be proven by contradiction: *)
-(*            we_time hv.t = ts *)
-(*            either hv ∈ h, then hv.t < ts *)
-(*            or hv ∉ h but hv ∈ Hk, then ts < hv.t *)
-      { admit. }
+      (** We prove it by contradiction. *)
+      { assert False.
+        destruct HcohM.
+        destruct (kvs_ValidSnapshotTimesCuts k Hk ts Hin Hts)
+          as (hl & hr & Hd & Hlt & Hgt).
+        assert (e ∈ hl ∨ e ∈ hr) as Habs.
+        { assert (e ∈ Hk) as Hke.
+          { assert (e ∈ (reverse Hk)).
+            { rewrite Hkeq. apply elem_of_list_here. }
+            by set_solver. }
+          by set_solver. }
+        destruct Habs as [Habs|Habs].
+        - specialize (Hlt e Habs); lia.
+        - specialize (Hgt e Habs); lia.
+        - done. }
       wp_pures.
+      (** Now we have two cases depending on whether the time of `e`
+          is smaller than the timestamp `t` or greater than it. *)
       case_bool_decide.
+      (** In the case `e.t < ts`, we return the element `e`. *)
       { wp_pures.
         iApply ("HΦ" $! (Some (we_val e))).
         iRight.
         iExists e.
         iSplit; first done.
+        (** It remains thus to prove that the element `e` is 
+            indeed the snapshot of the transaction that started at 
+            the time `ts`. *)
         iPureIntro.
+        (** We proceed by case analysis on whether e ∈ h ∨ e ∉ h. *)
         assert (e ∈ h ∨ e ∉ h) as [Heh|Heh].
         {  destruct (bool_decide (e ∈ h)) eqn:Heh.
            - apply bool_decide_eq_true in Heh; set_solver.
            - apply bool_decide_eq_false in Heh; set_solver.
         }
+        (** Case (e ∈ h). The proof boils down to show that 
+         the current history Hk and the snapshot history 
+         are indeed the same, which intuitively true, since 
+         e is the most recent element in the current history Hk. *)
         - rewrite /hist_to_we.
           rewrite -head_reverse.
           assert (Some e = head (reverse Hk)) as Hek.
           { rewrite Hkeq. by list_simplifier. }
           rewrite Hek.
-          assert (h = Hk) as ->.
-          { destruct HcohM.
-            destruct (kvs_ValidSnapshotTimesCuts k Hk ts Hin Hts)
+          do 2 f_equal.
+          (** To prove this equality, we use the cuts invariant of the 
+           coherence of the database together with the fact that the 
+           snapshot of the transaction is valid, i.e. 
+           ∀ e : events.write_event, e ∈ h → we_time e < ts. *)
+          destruct HcohM.
+          destruct (kvs_ValidSnapshotTimesCuts k Hk ts Hin Hts)
               as (hl & hr & (Hc1 & Hc2 & Hc3)).
-            destruct Hpre as (hr' & Hc1').
-            apply Inj_instance_2.
-            (** State, prove, and use a lemma about unicity of half-cut. *)
+          destruct Hpre as (hr' & Hc1').
+          apply Inj_instance_2.
+          (** State, prove, and use a lemma about unicity of half-cut. *)
             admit.
-          }
-          done.
-        - destruct HcohM.
+        - (** Case (e ∉ h). This case is absurd, since by hypotheses, 
+          we have e.t < ts, h prefix of Hk and e ∈ Hk. To show the 
+          contradiction, we will use again the cut property. *)
+          destruct HcohM.
           destruct (kvs_ValidSnapshotTimesCuts k Hk ts Hin Hts)
             as (hl & hr & (Hc1 & Hc2 & Hc3)).
           (** Prove by absurdity, using again the fact that hl = h. *)
           admit. }
+      (** Finally, if ts < e.t, that means that e is a more recent element
+       which is thus not in the snapshot of the current transaction, so 
+       we conclude the proof by induction hypothesis. *)
       wp_pure _.
       iApply ("IH" $! ($(reverse Hr')) (<[ k := Hr']> M) Hr'
                with "[][][//][][$HΦ]").
       + iPureIntro. by rewrite lookup_insert. 
       + iPureIntro. list_simplifier.
         (* h prefix of Hk = Hr' ++ [e] and e ∉ h *) admit.
-      + (* State weakining lemma. *) admit.
+      + (** Use a weakining lemma. *)
+        admit.
   Admitted.
 
   Lemma read_handler_spec
@@ -232,6 +271,7 @@ Section Proof_of_read_handler.
   ip_of_address KVS_address] {{ v, Φ v }}.
   Proof.
     iIntros (Hin Hreqd Hts) "#Hlk #HGlobInv #HsnapT #HsnapH HΦ".
+    (** We start by acquiring the lock and getting the lock invariant. *)
     wp_apply (acquire_spec with "[Hlk]"); first by iFrame "#".
     iIntros (?) "(-> & Hlock & Hlkres)".
     wp_pures.
@@ -239,6 +279,8 @@ Section Proof_of_read_handler.
       as (kvsV T Tss m M Hmap Hvalid Hforall)
            "(HmemLoc & HtimeLoc & #Hstarts & HkvsL & HvnumL)".
     wp_load.
+    (** We also open the global system invariant as we will need 
+        to unify variables M and Mg and T and Tg, and get additional pure facts. *)
     wp_apply fupd_aneris_wp.
     iInv KVS_InvName
       as (Mg Tg Tssg gMg)
@@ -260,6 +302,7 @@ Section Proof_of_read_handler.
      { by iDestruct (mono_nat.mono_nat_auth_own_agree
                    with "[$HtimeGlob][$HtimeLoc]")
          as "(_ & <-)". }
+     (** We can now close the global invariant. *)
     iSplitL "HtimeGlob HmemG HmemGmono Hccls HtimeStartsGlob".
     { iModIntro. iNext.
       iExists _, _, _, _.
@@ -267,7 +310,9 @@ Section Proof_of_read_handler.
     }
     iModIntro.
     iModIntro.
+    (** Next, we apply the lemma above to get the most recent element (if any) from the DB. *)
     wp_apply (kvs_get_last_spec k ts T Tss Tssg kvsV h Hk); try eauto.
+    (** We can now release the lock and conclude the proof in each of two cases. *)
     iIntros (vo) "[(-> & ->)|Hres]".
     - wp_pures.
       wp_smart_apply (release_spec with "[-HΦ]").
@@ -279,22 +324,13 @@ Section Proof_of_read_handler.
       wp_pures.
       iApply ("HΦ" $! _ _).
       iSplit. 
-      -- iPureIntro.
-         simplify_eq /=.
-         eapply sum_is_ser_valid.
-         rewrite /sum_is_ser.
-         eexists (InjLV #()), _.
-         left.
-         split; first eauto.
-         simpl. split; last done.
-         by left.
+      -- iPureIntro. simplify_eq /=.
+         eapply sum_is_ser_valid. rewrite /sum_is_ser.
+         eexists (InjLV #()), _. left.  split; first eauto.
+         simpl. split; last done. by left.
       -- rewrite /ReqPost.
-         iFrame "#".
-         iLeft.
-         iExists k, ts, [], NONEV.
-         iFrame "#∗".
-         iPureIntro.
-         split_and!; eauto.
+         iFrame "#". iLeft. iExists k, ts, [], NONEV.
+         iFrame "#∗". iPureIntro. split_and!; eauto.
     - iDestruct "Hres" as (e) "(-> & %Hhist)".
       wp_pures.
       wp_smart_apply (release_spec with "[-HΦ]").
@@ -306,30 +342,18 @@ Section Proof_of_read_handler.
       wp_pures.
       iApply ("HΦ" $! _ _).
       iSplit.
-      -- iPureIntro.
-         simplify_eq /=.
-         exists (SOMEV (we_val e)).
-         left.
-         split; first done.
-         left.
-         exists (we_val e).
-         split; first done.
+      -- iPureIntro. simplify_eq /=.
+         exists (SOMEV (we_val e)). left. split; first done.
+         left. exists (we_val e). split; first done.
          specialize (map_Forall_lookup_1 _ M k Hk Hforall Hkin).
          simpl. intros Hser1.
          destruct (Forall_forall (λ we, KVS_Serializable (we.(we_val))) Hk)
-           as [Hs1 _].
-         specialize (Hs1 Hser1).
-         apply Hs1.
-         rewrite /hist_to_we in Hhist.
-         apply last_Some_elem_of in Hhist.
+           as [Hs1 _].  specialize (Hs1 Hser1). apply Hs1.
+         rewrite /hist_to_we in Hhist. apply last_Some_elem_of in Hhist.
          by eapply elem_of_prefix.
-      -- rewrite /ReqPost.
-         iFrame "#".
-         iLeft.
-         iExists k, ts, h, _.
-         iFrame "#∗".
-         iPureIntro.
-         split_and!; eauto.
+      -- rewrite /ReqPost. iFrame "#". iLeft.
+         iExists k, ts, h, _. iFrame "#∗".
+         iPureIntro. split_and!; eauto.
   Qed.
 
 End Proof_of_read_handler.
