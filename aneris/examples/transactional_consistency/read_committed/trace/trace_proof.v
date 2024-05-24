829702b815bac786dbf7d5b45809ca4a6b9eb8ea
Alnor <anders.alnor.mathiasen@gmail.com>
2024-05-24 13:53:03 +0200

    WIP trace proofs

--- aneris/examples/transactional_consistency/read_committed/trace/trace_proof.v
+++ aneris/examples/transactional_consistency/read_committed/trace/trace_proof.v
@@ -8,21 +8,21 @@ From aneris.aneris_lang.lib Require Import list_proof inject lock_proof.
 From aneris.aneris_lang.lib.serialization Require Import serialization_proof.
 From aneris.aneris_lang.program_logic Require Import lightweight_atomic.
 From aneris.examples.transactional_consistency Require Import code_api wrapped_library.
-From aneris.examples.transactional_consistency.read_uncommitted.specs Require Import specs resources.
+From aneris.examples.transactional_consistency.read_committed.specs Require Import specs resources.
 From aneris.examples.transactional_consistency Require Import user_params aux_defs state_based_model.
 
 Section trace_proof.
   Context `{!anerisG Mdl Σ, !User_params}.
 
   (** Wrapped resources  *)
-  Global Program Instance wrapped_resources `(res : !RU_resources Mdl Σ) : RU_resources Mdl Σ :=
+  Global Program Instance wrapped_resources `(res : !RC_resources Mdl Σ) : RC_resources Mdl Σ :=
     {|
       GlobalInv := True%I;
       OwnMemKey k V := True%I;
       OwnLocalKey k c vo := True%I;
       ConnectionState c s sa := True%I;
       IsConnected c sa := True%I;
-      KVS_ru := KVS_ru;
+      KVS_rc := KVS_rc;
       KVS_Init := True%I;
       KVS_ClientCanConnect sa := True%I;
       Seen k V := True%I;
@@ -37,9 +37,9 @@ Section trace_proof.
   Admitted.
 
   (* Library specification on the form required by the trace infrastructure *)
-  Definition ru_library_spec (clients : gset socket_address) (P0 : iProp Σ) 
+  Definition rc_library_spec (clients : gset socket_address) (P0 : iProp Σ) 
   (lib : KVS_transaction_api) : iProp Σ := 
-      (P0 -∗ ∃ (res : RU_resources Mdl Σ),
+      (P0 -∗ ∃ (res : RC_resources Mdl Σ),
                ([∗ set] k ∈ KVS_keys, k ↦ₖ ∅) ∗ 
                KVS_Init ∗ 
                GlobalInv ∗
@@ -54,8 +54,8 @@ Section trace_proof.
   (* Primary lemma to be used with adequacy *)
   Lemma library_implication (clients : gset socket_address) 
   (N : namespace) (P0 : iProp Σ) (lib : KVS_transaction_api) :
-    ru_library_spec clients P0 lib -∗
-    ru_library_spec clients (P0 ∗ trace_is [] ∗ trace_inv N valid_trace_ru) 
+    rc_library_spec clients P0 lib -∗
+    rc_library_spec clients (P0 ∗ trace_is [] ∗ trace_inv N valid_trace_rc) 
                             (KVS_wrapped_api lib).
   Proof.
   Admitted.
--- aneris/examples/transactional_consistency/snapshot_isolation/trace/trace_proof.v
+++ aneris/examples/transactional_consistency/snapshot_isolation/trace/trace_proof.v
@@ -8,23 +8,24 @@ From aneris.aneris_lang.lib Require Import list_proof inject lock_proof.
 From aneris.aneris_lang.lib.serialization Require Import serialization_proof.
 From aneris.aneris_lang.program_logic Require Import lightweight_atomic.
 From aneris.examples.transactional_consistency Require Import code_api wrapped_library.
-From aneris.examples.transactional_consistency.read_uncommitted.specs Require Import specs resources.
+From aneris.examples.transactional_consistency.snapshot_isolation.specs Require Import specs resources.
 From aneris.examples.transactional_consistency Require Import user_params aux_defs state_based_model.
 
 Section trace_proof.
   Context `{!anerisG Mdl Σ, !User_params}.
 
   (** Wrapped resources  *)
-  Global Program Instance wrapped_resources `(res : !RU_resources Mdl Σ) : RU_resources Mdl Σ :=
+  Global Program Instance wrapped_resources `(res : !SI_resources Mdl Σ) : SI_resources Mdl Σ :=
     {|
       GlobalInv := True%I;
       OwnMemKey k V := True%I;
       OwnLocalKey k c vo := True%I;
       ConnectionState c s sa := True%I;
       IsConnected c sa := True%I;
-      KVS_ru := KVS_ru;
+      KVS_si := KVS_si;
       KVS_Init := True%I;
       KVS_ClientCanConnect sa := True%I;
+      KeyUpdStatus v k b := True%I;
       Seen k V := True%I;
     |}.
   Next Obligation.
@@ -37,10 +38,10 @@ Section trace_proof.
   Admitted.
 
   (* Library specification on the form required by the trace infrastructure *)
-  Definition ru_library_spec (clients : gset socket_address) (P0 : iProp Σ) 
+  Definition si_library_spec (clients : gset socket_address) (P0 : iProp Σ) 
   (lib : KVS_transaction_api) : iProp Σ := 
-      (P0 -∗ ∃ (res : RU_resources Mdl Σ),
-               ([∗ set] k ∈ KVS_keys, k ↦ₖ ∅) ∗ 
+      (P0 -∗ ∃ (res : SI_resources Mdl Σ),
+               ([∗ set] k ∈ KVS_keys, k ↦ₖ []) ∗ 
                KVS_Init ∗ 
                GlobalInv ∗
                ([∗ set] sa ∈ clients, KVS_ClientCanConnect sa) ∗
@@ -54,8 +55,8 @@ Section trace_proof.
   (* Primary lemma to be used with adequacy *)
   Lemma library_implication (clients : gset socket_address) 
   (N : namespace) (P0 : iProp Σ) (lib : KVS_transaction_api) :
-    ru_library_spec clients P0 lib -∗
-    ru_library_spec clients (P0 ∗ trace_is [] ∗ trace_inv N valid_trace_ru) 
+    si_library_spec clients P0 lib -∗
+    si_library_spec clients (P0 ∗ trace_is [] ∗ trace_inv N valid_trace_si) 
                             (KVS_wrapped_api lib).
   Proof.
   Admitted.
