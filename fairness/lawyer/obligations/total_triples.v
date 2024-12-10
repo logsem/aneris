43e99c96bded7566be8041c2119a49e906853d7f
fresheed <ok.john.rus@gmail.com>
2024-12-10 10:33:29 +0100

    Defined fair lock spec

--- fairness/lawyer/obligations/total_triples.v
+++ fairness/lawyer/obligations/total_triples.v
@@ -40,24 +40,23 @@ Section TotalTriples.
 
   Section AtomicTriples. 
     Context
-      (τ: Locale) (π: Phase)(* TODO: should it be fixed? *)
+      (τ: Locale)(* TODO: should it be fixed? *)
       {ST: Type}
-      (P: ST -> iProp Σ) (Q: val -> ST -> iProp Σ)
+      (P: ST -> iProp Σ) (Q: ST -> ST -> iProp Σ) (* second ST is the previous state *)
       (L: gset Level) (* TODO: only finite sets? *)
       {RO: Type}
       (round: ST -> RO) (* TODO: can we get away with ST only? *)
-      (TGT: ST -> Prop) `{forall x, Decision (TGT x)}
-      (d__h d__l: Degree)
-      (c: nat)
-      (d__m: Degree)
+      (TGT: ST -> Prop) (* `{forall x, Decision (TGT x)} *)
+      (d__h d__l d__m: Degree)
+      (c: nat) (B: nat -> nat)
       (ε__m: coPset)
-      (B: nat -> nat)
     .
 
     Section AtomicUpdates.
       Context
         (ε: coPset)
-        (Φ: val -> iProp Σ)
+        (π: Phase)
+        (Φ: iProp Σ)
         (O: gset SignalId)
         (RR: option RO -> iProp Σ)
       .
@@ -69,7 +68,7 @@ Section TotalTriples.
         |={ε, ∅}=> ∃ x, P x ∗ (
                            let abort := P x ={∅, ε}=∗ V in
                            ((∃ r__p, RR r__p) ∗ ⌜ TGT x ⌝ ∗ obls τ O (oGS := oGS) -∗
-                              BMU ∅ c (oGS := oGS) (∀ v, Q v x ={∅, ε}=∗ Φ v)) ∧
+                              BMU ∅ c (oGS := oGS) (∀ y, Q y x ={∅, ε}=∗ Φ)) ∧
                              abort
                          ).
       
@@ -97,7 +96,7 @@ Section TotalTriples.
                       )
               ) ∧
               ((∃ r__p, RR r__p) ∗ ⌜ TGT x ⌝ ∗ obls τ O (oGS := oGS) -∗
-               BMU ∅ c (oGS := oGS) (∀ v, Q v x ={∅, ε}=∗ Φ v)) ∧
+               BMU ∅ c (oGS := oGS) (∀ y, Q y x ={∅, ε}=∗ Φ)) ∧
               abort
       ).
       
@@ -111,18 +110,81 @@ Section TotalTriples.
     Context `{hGS: @heapGS Σ _ EM}.
     Let eGS: em_GS Σ := heap_fairnessGS (heapGS := hGS).
 
-    Let TLAT__impl (tau: coPset → (val → iProp Σ) → gset SignalId → (option RO → iProp Σ) -> iProp Σ)
+    Let TLAT__impl (tau: coPset → Phase -> iProp Σ → gset SignalId → (option RO → iProp Σ) -> iProp Σ)
       (e: expr) (s: stuckness): iProp Σ :=
-      ∀ (Φ: val -> iProp Σ) (O: gset SignalId) (RR: option RO -> iProp Σ),
+      ∀ (Φ: iProp Σ) (π: Phase) (O: gset SignalId) (RR: option RO -> iProp Σ),
         let ε := ⊤ ∖ ε__m in
         ⌜ B c <= LIM_STEPS ⌝ -∗
-        RR None ∗ cp π d__m (oGS := oGS) ∗ obls τ O (oGS := oGS) ∗ sgns_level_gt' O L -∗
-        tau ε Φ O RR -∗
-        WP e @ s; τ; ⊤ {{ v, Φ v }}.
+        RR None ∗ obls τ O (oGS := oGS) ∗ sgns_level_gt' O L ∗
+        th_phase_ge τ π (oGS := oGS) ∗ cp π d__m (oGS := oGS) -∗
+        tau ε π Φ O RR -∗
+        WP e @ s; τ; ⊤ {{ _, Φ }}.
     
     Definition TLAT1 := TLAT__impl TAU1. 
-    Definition TLAT2 := TLAT__impl TAU2. 
+    Definition TLAT2 := TLAT__impl TAU2.
 
   End AtomicTriples.
 
 End TotalTriples.
+
+
+(* TODO: move to another file *)
+Section FairLockSpec.
+  Context `{OfeDiscrete DegO} `{OfeDiscrete LevelO}.
+  Context `{@LeibnizEquiv (ofe_car LevelO) (ofe_equiv LevelO)}. 
+  
+  Let Degree := ofe_car DegO.
+  Let Level := ofe_car LevelO.
+
+  Context `{OPRE: ObligationsParamsPre Degree Level LIM_STEPS}.
+  Let OP := LocaleOP (Locale := locale heap_lang).
+  Existing Instance OP.
+  Let OM := ObligationsModel.
+  
+  Context {Σ: gFunctors}.
+  Context {invs_inΣ: invGS_gen HasNoLc Σ}.
+
+  Let OAM := ObligationsAM. 
+  Let ASEM := ObligationsASEM.
+  Context {oGS: @asem_GS _ _ ASEM Σ}.
+
+  Context `{EM: ExecutionModel heap_lang M}. 
+  Context `{hGS: @heapGS Σ _ EM}.
+  Let eGS: em_GS Σ := heap_fairnessGS (heapGS := hGS).    
+
+  Record FairLock := {
+    fl_create: expr; fl_acquire: expr; fl_release: expr;
+    fl_c__cr: nat; fl_B: nat -> nat;
+    fl_LK: val * nat * bool -> iProp Σ;
+    fl_lvls: gset Level;
+    fl_d__h: Degree;
+    fl_d__l: Degree;
+    fl_d__m: Degree;
+    fl_degs_lh: deg_lt fl_d__l fl_d__h;
+    fl_degs_hm: deg_lt fl_d__h fl_d__m;
+    fl_ι: namespace;
+    fl_create_spec: ⊢ ⌜ fl_c__cr <= LIM_STEPS ⌝ -∗
+      ∀ τ c,
+      {{{ ⌜ True ⌝ }}} fl_create #() @ τ {{{ lk, RET lk; fl_LK (lk, 0, false) ∗
+        
+        TLAT2 τ (fun x => fl_LK x ∗ ⌜ x.1.1 = lk ⌝) (fun y x => fl_LK y ∗ ⌜ y = x /\ x.2 = false ⌝)
+          fl_lvls
+          (fun '(_, r, _) => r) (fun '(_, _, b) => b = false)
+          fl_d__h fl_d__l fl_d__m
+          c fl_B                        
+          (↑ fl_ι) (fl_acquire #()) NotStuck
+          (oGS := oGS) ∗
+        
+        TLAT1 τ (fun x => fl_LK x ∗ ⌜ x.2 = true /\ x.1.1 = lk⌝) (fun y x => fl_LK y ∗ ⌜ y.1.2 = (x.1.2 + 1)%nat /\ y.2 = false /\ y.1.1 = x.1.1 ⌝)
+          fl_lvls
+          (fun '(_, r, _) => r) (fun _ => True%type)
+          fl_d__h fl_d__l fl_d__m
+          c fl_B
+          (↑ fl_ι) (fl_release #()) NotStuck
+          (oGS := oGS)
+      }}}
+  }. 
+
+End FairLockSpec.
+
+
