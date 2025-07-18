1406eb4fb93b0f0cdb51d8c362bc1d7e124b9b2b
fresheed <ok.john.rus@gmail.com>
2025-07-18 11:57:20 +0200

    moved trace ctx defs to a separate file

---
+++ lawyer/nonblocking/trace_context.v
@@ -0,0 +1,18 @@
+From trillium.traces Require Import exec_traces trace_lookup. 
+From trillium.program_logic Require Import language adequacy.
+
+Section TraceContext.
+  Context `{EqDecision (locale Λ)}.
+
+  Record trace_ctx := TraceCtx {
+    tctx_index: nat;
+    tctx_tid: locale Λ;
+    tctx_ctx: ectx Λ;
+  }.
+  
+  Context (tr: extrace Λ). 
+
+  Definition expr_under '(TraceCtx i τ K) (e: expr Λ) :=
+    exists c, tr S!! i = Some c /\ from_locale c.1 τ = Some (ectx_fill K e).
+
+End TraceContext.
