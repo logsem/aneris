871dbff7a9fac40c703bfa3eda54028933a8481d
fresheed <ok.john.rus@gmail.com>
2025-07-22 19:04:55 +0200

    wip: defining PR for wait-freedom proofs

--- lawyer/nonblocking/obligations_wfree_em.v
+++ lawyer/nonblocking/obligations_wfree_em.v
@@ -1,85 +1,67 @@
-From iris.proofmode Require Import tactics.
-From trillium.traces Require Import inftraces trace_lookup exec_traces trace_len. 
-From trillium.program_logic Require Import execution_model weakestpre adequacy. 
-From fairness Require Import fairness.
-From lawyer.examples Require Import orders_lib.
-From lawyer.obligations Require Import obligations_resources obligations_model obligations_em.
+(* From iris.proofmode Require Import tactics. *)
+(* From trillium.traces Require Import inftraces exec_traces trace_len.  *)
+(* From trillium.program_logic Require Import execution_model weakestpre adequacy.  *)
+(* From fairness Require Import fairness. *)
+(* From lawyer.examples Require Import orders_lib. *)
+(* From lawyer.obligations Require Import obligations_resources obligations_model obligations_em. *)
 
 
-Section TraceContext.
-  Context `{EqDecision (locale Λ)}.
+(* Section ObligationsWaitFreeEM. *)
+(*   Context `{Countable (locale Λ)}. *)
 
-  Record trace_ctx := TraceCtx {
-    tctx_index: nat;
-    tctx_tid: locale Λ;
-    tctx_ctx: ectx Λ;
-  }.
+(*   Context (ic: @trace_ctx Λ). (** location of the infinite call *) *)
+
+
+
+(*   Definition obls_wfree_is_init_st (σ: cfg Λ) (δ: mstate OM): Prop. *)
+(*   Admitted. *)
+(*     (* obls_is_init_st σ δ /\ *) *)
+    
+(*   Lemma obls_wfree_resources_init Σ {PRE: ObligationsPreGS Σ}: *)
+(*     ∀ s1 σ p (INIT: obls_is_init_st σ s1), *)
+(*         ⊢ |==> ∃ eGS: ObligationsGS Σ, obls_init_resource s1 p ∗ obls_wfree_ti {tr[ σ ]} {tr[ s1 ]}. *)
+(*   Proof using. *)
+(*     clear H1 H0 H.  *)
+(*     simpl. intros δ σ ? INIT. iStartProof. *)
+(*     iMod (own_alloc (● (cps_repr $ ps_cps  δ) ⋅ ◯ _)) as (?) "[CPa CPf]". *)
+(*     { by apply auth_both_valid_2. } *)
+(*     iMod (own_alloc (● (sig_map_repr (ps_sigs δ)) ⋅ ◯ _)) as (?) "[SIGSa SIGSf]". *)
+(*     { apply auth_both_valid_2; [| reflexivity]. *)
+(*       rewrite /sig_map_repr. *)
+(*       intros s. destruct lookup eqn:L; [| done]. *)
+(*       apply lookup_fmap_Some in L.  *)
+(*       destruct L as ([l b]&<-&?). *)
+(*       done. } *)
+(*     iMod (own_alloc (● (obls_map_repr $ ps_obls δ) ⋅ ◯ _)) as (?) "[OBLSa OBLSf]".  *)
+(*     { apply auth_both_valid_discrete. split; [reflexivity| ]. *)
+(*       intros τ. rewrite lookup_fmap.  *)
+(*       by destruct lookup. } *)
+(*     iMod (own_alloc (● (eps_repr $ ps_eps δ) ⋅ ◯ _)) as (?) "[EPSa EPSf]".  *)
+(*     { by apply auth_both_valid_2. } *)
+
+(*     iMod (ghost_map_alloc (wrap_phase <$> ps_phases δ)) as (?) "[PHa PHf]". *)
+(*     iMod (own_alloc (●MN (ps_exc_bound δ) ⋅ mono_nat_lb _)) as (?) "[LBa LBf]". *)
+(*     { apply mono_nat_both_valid. reflexivity. } *)
+(*     iModIntro. iExists {| obls_pre := PRE; |}. *)
+(*     rewrite big_sepM_fmap. iFrame. *)
+
+(*     rewrite /cps_repr. iSplitL. *)
+(*     { by iApply cps_mset. }  *)
+
+(*     iPureIntro.  *)
+(*     red in INIT. destruct INIT as (?&?). *)
+(*     red. rewrite /threads_own_obls /dom_phases_obls. *)
+(*     erewrite om_wf_dpo; eauto. *)
+(*   Qed. *)
   
-  Context (tr: extrace Λ). 
-
-  Definition expr_under '(TraceCtx i τ K) (e: expr Λ) :=
-    exists c, tr S!! i = Some c /\ from_locale c.1 τ = Some (ectx_fill K e).
-
-End TraceContext.
-
-
-Section ObligationsWaitFreeEM.
-  Context `{Countable (locale Λ)}.
-
-  Context (ic: @trace_ctx Λ). (** location of the infinite call *)
-  Context (F: nat). (** amount of fuel required for the call (currently at d0) *)
-
-  Definition WF_SB := 1.
-
-  Instance OP_WF: ObligationsParams (bounded_nat 2) unit (locale Λ) WF_SB.
-  esplit; try by apply _.
-  - apply nat_bounded_PO.
-  - apply unit_PO.
-  Defined.
-
-  Let OM := ObligationsModel.
-  Notation "'Degree'" := (bounded_nat 2). 
-  Notation "'Level'" := (unit).
-
-  Let d0: Degree := ith_bn 2 0 ltac:(lia).
-  Let d1: Degree := ith_bn 2 1 ltac:(lia).
-  Let l0: Level := tt.
-
-  Definition extra_fuel `{!ObligationsGS Σ} (omtr: auxiliary_trace OM) :=
-    let len := trace_length omtr in
-    let i := tctx_index ic in
-    if i <? len then (cp_mul π0 d0 (len - i) ∗ cp_mul π0 d0 F)%I else ⌜ True ⌝%I.
-
-  Definition cur_phases `{!ObligationsGS Σ} (omtr: auxiliary_trace OM): iProp Σ :=
-    let δ := trace_last omtr in
-    let τ := tctx_tid ic in
-    ([∗ map] τ' ↦ π' ∈ delete τ (ps_phases δ), th_phase_eq τ' π') ∗
-    let ph_res π := let q := if (tctx_index ic <? trace_length omtr) 
-                             then 1%Qp else (/2)%Qp
-                    in th_phase_frag τ π q in
-    from_option ph_res ⌜ True ⌝%I (ps_phases δ !! τ).
-
-  Definition cur_obls_sigs `{!ObligationsGS Σ} (omtr: auxiliary_trace OM): iProp Σ :=
-    let δ := trace_last omtr in
-    let τ := tctx_tid ic in
-    ([∗ set] τ' ∈ dom (ps_obls δ) ∖ {[ τ ]}, obls τ' ∅) ∗
-    if decide (τ ∈ dom (ps_obls δ))
-    then (∃ s, obls τ {[ s ]} ∗ sgn s l0 (Some false) ∗ ep s π0 d0)%I
-    else cp π0 d1. 
-
-  Definition obls_wfree_ti `{!ObligationsGS Σ}
-    (etr: execution_trace Λ) (omtr: auxiliary_trace OM): iProp Σ :=
-    obls_ti etr omtr ∗ extra_fuel omtr ∗ cur_phases omtr ∗ cur_obls_sigs omtr.
-
-  foobar. 
-  Definition ObligationsEM: ExecutionModel Λ OM :=
-    {| 
-      em_Σ := obls_Σ;
-      em_valid_evolution_step := obls_valid_evolution_step;
-      em_thread_post Σ `{!ObligationsGS Σ} :=
-        fun τ _ => (obls τ ∅)%I;
-      em_initialization := obls_resources_init;
-    |}.
+(*   Definition ObligationsEM: ExecutionModel Λ OM := *)
+(*     {|  *)
+(*       em_Σ := obls_Σ; *)
+(*       em_valid_evolution_step := obls_valid_evolution_step; *)
+(*       em_thread_post Σ `{!ObligationsGS Σ} := *)
+(*         fun τ _ => (obls τ ∅)%I; *)
+(*       em_initialization := obls_resources_init; *)
+(*     |}. *)
 
 
-End ObligationsWaitFreeEM.
+(* End ObligationsWaitFreeEM. *)
--- lawyer/nonblocking/pr_wfree.v
+++ lawyer/nonblocking/pr_wfree.v
@@ -1,51 +1,177 @@
 From iris.proofmode Require Import tactics.
 From trillium.traces Require Import inftraces trace_lookup exec_traces trace_len. 
-From trillium.program_logic Require Import execution_model weakestpre adequacy simulation_adequacy_em_cond. 
-From trillium.prelude Require Import classical.
-From fairness Require Import fairness.
+From trillium.program_logic Require Import execution_model weakestpre adequacy_utils adequacy_cond simulation_adequacy_em_cond. 
+From fairness Require Import fairness locales_helpers.
 From lawyer Require Import program_logic sub_action_em action_model.
-From lawyer.examples Require Import orders_lib obls_tactics.
-From lawyer.obligations Require Import obligations_resources obligations_logic env_helpers obligations_adequacy obligations_model obligations_em obligations_am obls_termination.
+(* From lawyer.examples Require Import orders_lib obls_tactics. *)
+From lawyer.obligations Require Import obligations_resources obligations_logic env_helpers obligations_adequacy obligations_model obligations_em obligations_am.
 From heap_lang Require Import lang.
-From lawyer.nonblocking Require Import trace_context.
-
-Definition LoopingModel: Model :=
-  {| mstate := unit; mlabel := unit; mtrans := fun _ _ _ => True |}.
-
-(* Class LoopIrisG {Λ: language} Σ := { *)
-(*     lig_inner :: irisG Λ LoopingModel Σ *)
-(* }. *)
+From lawyer.nonblocking Require Import trace_context om_wfree_inst.
 
 
-  (* Program Definition WptpPR {Σ} {Hinv : @IEMGS _ _ LG_EM EM Σ} *)
-  (*   (iG := IEM_irisG LG_EM EM) *)
-  (*   : ProgressResource state_interp (fun _ _ => ⌜ True ⌝%I) fork_post C := *)
-  (*   {| pr_pr := (fun s etr Φs => wptp s (trace_last etr).1 Φs) |}.  *)
-  (* Next Obligation.  *)
-  (*   intros. apply wptp_of_val_post. *)
-  (* Qed. *)
-  (* Next Obligation.  *)
-  (*   clear R FILTER_PCL C_DEC. *)
-  (*   intros. rewrite H0. *)
-  (*   iIntros "? WPS". *)
-  (*   iMod (wptp_not_stuck_same _ _ _ _ [] with "[$] [WPS]") as "(?&?&%NS)".  *)
-  (*   3: by iFrame. *)
-  (*   { eauto. } *)
-  (*   { erewrite app_nil_l, app_nil_r. *)
-  (*     erewrite <- surjective_pairing. apply trace_ends_in_last. } *)
-  (*   iModIntro. iFrame. *)
-  (*   iPureIntro. intros. subst. rewrite -H0. *)
-  (*   eapply NS; eauto. *)
-  (*   apply last_eq_trace_ends_in in H0. rewrite H0. simpl. set_solver. *)
-  (* Qed. *)
-  (* Final Obligation.  *)
-  (*   intros. *)
-  (*   iIntros "?? _ ? _". (** for usual wptp with the same TI, trivial trace invariant and any trace condition don't matter *) *)
-  (*   iMod (take_step with "[$] [$] [$]") as "X". *)
-  (*   1, 2: by eauto. *)
-  (*   { by rewrite H0. } *)
-  (*   iModIntro. iApply (step_fupdN_mono with "[$]"). *)
-  (*   iIntros "X". iMod "X" as "(?&?)". iModIntro. iFrame.  *)
-  (*   setoid_rewrite bi.True_sep'. by rewrite H0. *)
-  (* Qed. *)
+Close Scope Z.
 
+
+Section Pwp.
+
+  Definition LoopingModel: Model :=
+    {| mstate := unit; mlabel := unit; mtrans := fun _ _ _ => True |}.
+
+  (* Class LoopIrisG {Λ: language} Σ := { *)
+  (*   lig_inner :: irisG Λ LoopingModel Σ *)
+  (* }. *)
+
+  (* Definition pwp {Λ: language} {Σ: gFunctors} := @wp Λ (iProp Σ) LoopingModel. *)
+  (* Definition pwp := @wp (A := LoopingModel). *)
+  (* Definition pwp {Λ: language} {PROP} := @wp Λ PROP LoopingModel. *)
+  (* Global Arguments pwp {_ _ _}.  wp *)
+  Definition pwp `{!irisG Λ LoopingModel Σ} :=
+    @wp Λ (iProp Σ) stuckness _. 
+
+End Pwp. 
+
+
+Section WaitFreePR.
+
+  Let OP := LocaleOP (OPRE := OPP_WF) (Locale := locale heap_lang). 
+  Existing Instance OP.
+  Let OM := ObligationsModel.
+
+  Let M := AM2M ObligationsAM.
+  Let ASEM := ObligationsASEM.
+  Let EM := TopAM_EM ASEM (fun {Σ} {aGS: asem_GS Σ} τ _ => obls τ ∅ (oGS := aGS)).
+
+  Context (ic: @trace_ctx heap_lang).
+  Let ii := tctx_index ic.
+  Let Ki := tctx_ctx ic.
+  Let τi := tctx_tid ic.
+
+  Context (m: val).
+  Context (F: nat). (** amount of fuel required for the call (currently at d0) *)
+
+  Definition no_extra_obls (_: cfg heap_lang) (δ: mstate M) :=
+    forall τ', default ∅ (ps_obls δ !! τ') ≠ ∅ -> τ' = τi.
+
+  Definition obls_sim_rel_wfree extr omtr :=
+    obls_sim_rel extr omtr /\ no_extra_obls (trace_last extr) (trace_last omtr).
+
+  Definition wfree_trace_inv `{Hinv : @IEMGS _ _ HeapLangEM EM Σ}
+    (extr: execution_trace heap_lang) (omtr: auxiliary_trace M): iProp Σ :=
+    ⌜ no_extra_obls (trace_last extr) (trace_last omtr) ⌝.
+
+  Definition fits_inf_call: execution_trace heap_lang → Prop.
+  Admitted.
+
+  Definition phys_SI {Σ} {Hinv : @IEMGS _ _ HeapLangEM EM Σ}
+    (etr: execution_trace heap_lang) (_: auxiliary_trace LoopingModel): iProp Σ :=
+    lgem_si (trace_last etr).2 (lgem_GS0 := (iem_phys HeapLangEM EM)).
+
+  (* TODO: move *)
+  Definition wptp_from_gen {Σ: gFunctors}
+    (W: expr -> locale heap_lang -> (val → iPropI Σ) -> iProp Σ)
+    (t0 t: list expr) (Φs : list (val → iPropI Σ)) :=
+    ([∗ list] tp1_e;Φ ∈ (prefixes_from t0 t);Φs,
+       W tp1_e.2 (locale_of tp1_e.1 tp1_e.2) Φ)%I.
+
+  Definition wptp_gen {Σ} W t Φs := (@wptp_from_gen Σ W [] t Φs).
+
+  (** not making it an instance to avoid incorrect Iris instantiations *)
+  Definition iris_OM_into_Looping {Σ} (Hinv: @IEMGS _ _ HeapLangEM EM Σ):
+    irisG heap_lang LoopingModel Σ.
+  Proof using.
+    exact {| state_interp := phys_SI; fork_post := fun _ _ => (⌜ True ⌝)%I |}.
+  Defined.
+
+  (* TODO: move *)
+  Definition under_ctx (K: ectx heap_lang) (e: expr): option expr.
+  Admitted.
+
+  Lemma under_ctx_spec K e e':
+    under_ctx K e = Some e' <-> ectx_fill K e' = e.
+  Proof using. Admitted.
+
+  (* Definition empty_post {Σ: gFunctors}: val -> iProp Σ := (fun _ => ⌜ True ⌝%I).  *)
+
+  Definition wp_tc {Σ} {Hinv : @IEMGS _ _ HeapLangEM EM Σ}
+    (s: stuckness) (e: expr) (b: bool) Φ :=
+    if b then
+      let e' := default (Val #false) (under_ctx Ki e) in
+      wp s ⊤ τi e' Φ
+    else
+      let _ := iris_OM_into_Looping in
+      pwp s ⊤ τi e Φ.
+
+  Definition wptp_wfree {Σ} {Hinv : @IEMGS _ _ HeapLangEM EM Σ}
+    (s: stuckness)
+    (* (tp: list expr) *)
+    (etr: execution_trace heap_lang)
+    (Φs : list (val → iPropI Σ)):
+    iProp Σ :=
+    wptp_gen (fun e τ Φ => if decide (τi = τ) 
+                          then wp_tc s e (ii <? trace_length etr) Φ
+                          else
+                            let _ := iris_OM_into_Looping in
+                            pwp s ⊤ τ e Φ)
+      (trace_last etr).1 Φs.
+
+  Open Scope WFR_scope. 
+
+  Definition extra_fuel `{!ObligationsGS Σ} (etr: execution_trace heap_lang) :=
+    let len := trace_length etr in
+    if len <? ii then (cp_mul π0 d0 (ii - len) ∗ cp_mul π0 d0 F)%I else ⌜ True ⌝%I.
+
+  (* TODO: need to support the situation where 1/2 of τ's phase is taken out *)
+  (*      already in the starting state - use a token? *)
+  Definition cur_phases `{!ObligationsGS Σ} (etr: execution_trace heap_lang): iProp Σ :=
+    let c := trace_last etr in
+    (* ([∗ map] τ' ↦ π' ∈ delete τ (ps_phases δ), th_phase_eq τ' π') ∗ *)
+    ([∗ set] τ ∈ locales_of_cfg c ∖ {[ τi ]}, ∃ π, th_phase_eq τ π) ∗
+    let ph_res := let q := if (trace_length etr <? ii) then 1%Qp else (/2)%Qp in
+                  (∃ π, th_phase_frag τi π q)%I in
+    ⌜ τi ∈ locales_of_cfg c ⌝ → ph_res.
+
+  Definition cur_obls_sigs `{!ObligationsGS Σ} (etr: execution_trace heap_lang): iProp Σ :=
+    let c := trace_last etr in
+    ([∗ set] τ ∈ locales_of_cfg c ∖ {[ τi ]}, obls τ ∅) ∗
+    if decide (τi ∈ locales_of_cfg c)
+    then (∃ s, obls τi {[ s ]} ∗ sgn s l0 (Some false) ∗ ep s π0 d0)%I
+    else cp π0 d1.
+
+  (* Lemma foo {Σ} {Hinv : @IEMGS _ _ HeapLangEM EM Σ}: ObligationsGS Σ. *)
+  (*   apply Hinv. *)
+  (*   Show Proof.  *)
+  (*   apply _.  *)
+  
+  Definition pr_pr_wfree {Σ} {Hinv : @IEMGS _ _ HeapLangEM EM Σ}
+    (s: stuckness) (etr: execution_trace heap_lang) (Φs: list (val → iProp Σ)): iProp Σ :=
+    let oGS: ObligationsGS Σ := @iem_fairnessGS _ _ _ _ _ Hinv in
+    wptp_wfree s etr Φs ∗ extra_fuel etr ∗ cur_phases etr ∗ cur_obls_sigs etr. 
+
+(*   Definition obls_wfree_ti `{!ObligationsGS Σ} *)
+(*     (etr: execution_trace Λ) (omtr: auxiliary_trace OM): iProp Σ := *)
+(*     obls_ti etr omtr ∗ extra_fuel omtr ∗ cur_phases omtr ∗ cur_obls_sigs omtr. *)
+
+  Program Definition PR_wfree {Σ} {Hinv : @IEMGS _ _ HeapLangEM EM Σ}:
+    @ProgressResource heap_lang M Σ (@iem_invGS _ _ _ _ _ Hinv)
+      state_interp wfree_trace_inv fork_post fits_inf_call :=
+    {| pr_pr := pr_pr_wfree |}.
+  Next Obligation.
+    intros.
+    rewrite /pr_pr_wfree.
+    iIntros "(WPS & CPS & PH & OB)".
+    iAssert (|~~| (Ps ∗ (Ps -∗ wptp_wfree s ex Φs)))%I with "[WPS]" as "CLOS".
+    2: { iMod "CLOS". iModIntro. by iFrame. }
+    rewrite /wptp_wfree.
+    iDestruct (big_sepL2_length with "[$]") as "%LENS".
+    rewrite adequacy_utils.prefixes_from_length in LENS.
+    (* split thread pool into three parts, 
+       "frame" the outer two using wptp_of_val_post,
+       prove the remaining wp/pwp part for τi *)
+    admit.
+  Admitted.
+  Next Obligation.
+  Admitted. 
+  Final Obligation.
+  Admitted. 
+  
+End WaitFreePR.
