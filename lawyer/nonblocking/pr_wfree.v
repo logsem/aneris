9310f6970b84998c513fe43fd966e35ffc4a0d90
fresheed <ok.john.rus@gmail.com>
2025-07-23 11:14:24 +0200

    wip: proving waitfree pr

--- lawyer/nonblocking/pr_wfree.v
+++ lawyer/nonblocking/pr_wfree.v
@@ -66,12 +66,50 @@ Section WaitFreePR.
     (etr: execution_trace heap_lang) (_: auxiliary_trace LoopingModel): iProp Σ :=
     lgem_si (trace_last etr).2 (lgem_GS0 := (iem_phys HeapLangEM EM)).
 
-  (* TODO: move *)
-  Definition wptp_from_gen {Σ: gFunctors}
-    (W: expr -> locale heap_lang -> (val → iPropI Σ) -> iProp Σ)
-    (t0 t: list expr) (Φs : list (val → iPropI Σ)) :=
-    ([∗ list] tp1_e;Φ ∈ (prefixes_from t0 t);Φs,
-       W tp1_e.2 (locale_of tp1_e.1 tp1_e.2) Φ)%I.
+  Section WptpGen.
+    Context {Σ: gFunctors}.
+    Context (W: expr -> locale heap_lang -> (val → iPropI Σ) -> iProp Σ).
+    
+    (* TODO: move *)
+    Definition wptp_from_gen 
+      (t0 t: list expr) (Φs : list (val → iPropI Σ)) :=
+      ([∗ list] tp1_e;Φ ∈ (prefixes_from t0 t);Φs,
+         W tp1_e.2 (locale_of tp1_e.1 tp1_e.2) Φ)%I.
+
+    Lemma wptp_from_gen_app' t0 t1 Φs1 t2 Φs2
+      (MATCH1: length t1 = length Φs1):
+      wptp_from_gen t0 (t1 ++ t2) (Φs1 ++ Φs2) ⊢ wptp_from_gen t0 t1 Φs1 ∗ wptp_from_gen (t0 ++ t1) t2 Φs2. 
+    Proof using.
+      iIntros.
+      rewrite {1}/wptp_from_gen. rewrite prefixes_from_app. 
+      iDestruct (big_sepL2_app_inv with "[$]") as "(?&?)".
+      2: { iFrame. }
+      rewrite prefixes_from_length. tauto.  
+    Qed.
+
+    Lemma wptp_gen_split_1 t0 t1 t2 Φs:
+      wptp_from_gen t0 (t1 ++ t2) Φs ⊢
+      ⌜ exists Φs1 Φs2, Φs1 ++ Φs2 = Φs /\ length Φs1 = length t1 /\ length Φs2 = length t2 ⌝.
+    Proof using.
+      clear. 
+      iIntros "WPS".
+      iDestruct (big_sepL2_length with "[$]") as "%LENS".
+      rewrite adequacy_utils.prefixes_from_length in LENS.
+      iPureIntro.
+      do 2 eexists. split.
+      { apply (take_drop (length t1)). }
+      rewrite length_take. rewrite length_skipn.
+      rewrite length_app in LENS. lia.
+    Qed.
+
+    Lemma wptp_gen_singleton t0 e Φ:
+      wptp_from_gen t0 [e] [Φ] ⊣⊢ W e (locale_of t0 e) Φ. 
+    Proof using.
+      rewrite /wptp_from_gen. simpl.
+      by rewrite bi.sep_emp.
+    Qed.
+
+  End WptpGen.
 
   Definition wptp_gen {Σ} W t Φs := (@wptp_from_gen Σ W [] t Φs).
 
@@ -101,18 +139,17 @@ Section WaitFreePR.
       let _ := iris_OM_into_Looping in
       pwp s ⊤ τi e Φ.
 
+  Definition thread_pr {Σ} {Hinv : @IEMGS _ _ HeapLangEM EM Σ} s N :=
+    (fun e τ Φ => if decide (τi = τ) then wp_tc s e (ii <? N) Φ
+                 else let _ := iris_OM_into_Looping in pwp s ⊤ τ e Φ).
+
   Definition wptp_wfree {Σ} {Hinv : @IEMGS _ _ HeapLangEM EM Σ}
     (s: stuckness)
     (* (tp: list expr) *)
     (etr: execution_trace heap_lang)
     (Φs : list (val → iPropI Σ)):
     iProp Σ :=
-    wptp_gen (fun e τ Φ => if decide (τi = τ) 
-                          then wp_tc s e (ii <? trace_length etr) Φ
-                          else
-                            let _ := iris_OM_into_Looping in
-                            pwp s ⊤ τ e Φ)
-      (trace_last etr).1 Φs.
+    wptp_gen (thread_pr s (trace_length etr)) (trace_last etr).1 Φs.
 
   Open Scope WFR_scope. 
 
@@ -120,8 +157,6 @@ Section WaitFreePR.
     let len := trace_length etr in
     if len <? ii then (cp_mul π0 d0 (ii - len) ∗ cp_mul π0 d0 F)%I else ⌜ True ⌝%I.
 
-  (* TODO: need to support the situation where 1/2 of τ's phase is taken out *)
-  (*      already in the starting state - use a token? *)
   Definition cur_phases `{!ObligationsGS Σ} (etr: execution_trace heap_lang): iProp Σ :=
     let c := trace_last etr in
     (* ([∗ map] τ' ↦ π' ∈ delete τ (ps_phases δ), th_phase_eq τ' π') ∗ *)
@@ -157,7 +192,8 @@ Section WaitFreePR.
     {| pr_pr := pr_pr_wfree |}.
   Next Obligation.
     intros.
-    rewrite /pr_pr_wfree.
+    (* rewrite /pr_pr_wfree. *)
+    iUnfold pr_pr_wfree.
     iIntros "(WPS & CPS & PH & OB)".
     iAssert (|~~| (Ps ∗ (Ps -∗ wptp_wfree s ex Φs)))%I with "[WPS]" as "CLOS".
     2: { iMod "CLOS". iModIntro. by iFrame. }
@@ -172,6 +208,35 @@ Section WaitFreePR.
   Next Obligation.
   Admitted. 
   Final Obligation.
+    intros ??? etr Φs c oτ c' mtr VALID FIN STEP.
+    iIntros "#CWP TI #INV PR %FIT".
+    simpl. rewrite /obls_asem_mti. iDestruct "TI" as "(%EV & PHYS & MSI)".
+    inversion STEP as
+        [?? e σ e' σ' efs t1 t2 -> -> PSTEP | ].
+    2: { done. }
+    simpl in *. 
+    destruct oτ as [τ| ]; [| done]. inversion H0. clear H0.
+    rewrite H3 in STEP, FIT. rewrite H3.
+
+    rewrite /pr_pr_wfree. iDestruct "PR" as "(WPS & CPS & PH & OB)".
+    iUnfold wptp_wfree in "WPS".    
+    red in FIN. iEval (rewrite FIN; simpl) in "WPS".
+    iDestruct (wptp_gen_split_1 with "WPS") as %X.
+    destruct X as (Φs1 & Φs' & EQ & LEN1 & LEN').
+    iEval (rewrite -EQ) in "WPS".
+    iDestruct (wptp_from_gen_app' with "[$]") as "[WPS1 WPS'] /="; [done| ].
+    replace (e :: t2) with ([e] ++ t2) at 1 by done. 
+    iDestruct (wptp_gen_split_1 with "WPS'") as %X.
+    destruct X as (Φ_ & Φs2 & EQ' & LEN_ & LEN2).
+    iEval (rewrite -EQ') in "WPS'".
+    iDestruct (wptp_from_gen_app' with "WPS'") as "[WPS_ WPS2] /="; [done| ].
+    destruct Φ_ as [ | Φ [|]].
+    1, 3: simpl in LEN_; lia.
+    rewrite wptp_gen_singleton.
+
+    
+    
+  
   Admitted. 
   
 End WaitFreePR.
