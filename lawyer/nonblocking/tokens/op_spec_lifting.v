9f4085f0dea965573ffa84d4dc36ded62085652e
fresheed <ok.john.rus@gmail.com>
2025-11-24 12:52:41 +0100

    proved lifting up to "no forking" admit

--- lawyer/nonblocking/tokens/op_spec_lifting.v
+++ lawyer/nonblocking/tokens/op_spec_lifting.v
@@ -42,6 +42,160 @@ Section SpecLifting.
     iApply own_update. apply excl_auth_update. 
   Qed.
 
+  Lemma inside_call_extend extr tp1 tp2 K e σ1
+    (Hζ : locale_of tp1 (fill K e) = τ)
+    (Hextr : trace_ends_in extr (tp1 ++ fill K e :: tp2, σ1))
+    (s : nat)
+    (CALL : inside_call m (tpctx_tid (tctx_tpctx mock_tctx)) K s extr e)
+    (e2 : expr)
+    (σ2 : state)
+    (H : ectx_language.prim_step e σ1 e2 σ2 [])
+    (V2 : to_val e2 = None):
+    inside_call m τ K s (extr :tr[ Some τ ]: (tp1 ++ fill K e2 :: tp2 ++ [], σ2)) e2.
+  Proof using.
+    red in CALL. destruct CALL as (a & CALL & PREV & NORET & CUR).
+    pose proof (trace_length_at_least extr) as LENnz. simpl in LENnz.
+    remember (trace_length extr) as N.
+    red. exists a. simpl. rewrite -HeqN. rewrite -HeqN in LENnz PREV.
+     repeat split.
+    - destruct CALL as (?&CALL&?). eexists. split; eauto.
+      rewrite -CALL. apply trace_lookup_extend_lt. rewrite -HeqN. lia.
+    - lia.
+    - simpl. intros j DOM.
+      assert (s <= j <= trace_length extr - 1 \/ j = trace_length extr) as [DOM' | ->].
+      { lia. }
+      + pose proof DOM' as X%NORET. 
+        destruct (extr !! j) eqn:JTH; rewrite JTH /= in X; [| done].
+        rewrite trace_lookup_extend_lt.
+        2: { rewrite -HeqN. lia. }
+        rewrite JTH. eauto.
+      + rewrite trace_lookup_last.
+        2: { simpl. rewrite -HeqN. lia. }
+        simpl.
+        red. eexists. split; [| apply V2]. 
+        red.
+        rewrite -Hζ. rewrite /from_locale.
+        by rewrite from_locale_from_locale_of.
+    - rewrite app_nil_r.
+      rewrite -Hζ. rewrite /locale_of.
+      rewrite list_lookup_middle; [| done].
+      simpl. by apply under_ctx_spec.
+  Qed. 
+
+  Lemma lift_call e
+    (NVAL: to_val e = None):
+    (let _ := IEMGS_into_Looping (@pwt_Hinv _ PWT) si_add_none in
+     WP e @τ {{ _, unit_tok }}) -∗
+    ct_frag (Some e) -∗
+    (let _ := IEMGS_into_Looping (@pwt_Hinv _ PWT) (@ct_interp_tok mock_tctx m _ PWT) in WP e @τ {{ _, ct_frag None }}).
+  Proof using.
+    simpl. 
+    iIntros "WP MRK".
+    iLöb as "IH" forall (e NVAL).
+
+    repeat rewrite wp_unfold.
+    rewrite /wp_pre. simpl.
+    rewrite NVAL /=. 
+
+    iIntros (extr atr K tp1 tp2 σ1 Hvalid Hζ Hextr) "(PHYS & CTI)".
+
+    rewrite /ct_interp_tok /ct_interp. iDestruct "CTI" as "(%&AUTH & X)".
+    iDestruct (ct_auth_frag_agree with "[$] [$]") as %EQ. 
+    iDestruct "X" as "[(?&%) | (%&%&%&->&%CALL)]".
+    { subst. done. }
+    inversion EQ. subst e0. clear EQ.
+
+    assert (K0 = K) as ->.
+    { red in CALL. destruct CALL as (?&?&?&?&CUR). revert CUR. 
+      rewrite Hextr /=. 
+      subst τ. rewrite /locale_of. rewrite list_lookup_middle /=; [| done].
+      rewrite under_ctx_spec.
+      intros EQ%ectx_fill_ctx_nval; done. }    
+
+    iMod ("WP" $! _ looping_trace K with "[//] [] [] [PHYS]") as "[RED WP]".
+    1, 2: done.
+    { iFrame. }
+    iModIntro. iFrame.
+
+    iIntros "**".
+    iSpecialize ("WP" with "[//]"). 
+    iMod "WP". iModIntro.
+    iNext. 
+    iMod "WP". iModIntro.
+
+    iApply (@step_fupdN_wand with "WP"). iIntros "ST".
+    iMod "ST" as "(%&%& (PHYS & ADD) & WP & WPS)".
+
+    destruct (to_val e2) eqn:V2.
+    { apply of_to_val in V2 as <-. 
+      iPoseProof (wp_value_inv with "WP") as "POST".
+      iMod (pre_step_elim with "[PHYS ADD] POST") as "((PHYS & ADD) & TOK)".
+      { iFrame. }
+      iMod (ct_auth_frag_update _ _ None with "[$] [$]") as "[??]".      
+      iModIntro. do 2 iExists _. iFrame.
+      assert (efs = []) as -> by admit. repeat rewrite big_sepL_nil.
+      iSplitL "TOK".
+      { iLeft. by iFrame. }
+      iSplit; [| done]. by iApply @wp_value.
+      Unshelve. 2, 3: exact tt. exact looping_trace. }
+
+    iMod (ct_auth_frag_update _ _ (Some e2) with "[$] [$]") as "[AUTH FRAG]". 
+    iModIntro. do 2 iExists _. iFrame.
+    assert (efs = []) as -> by admit. repeat rewrite big_sepL_nil.
+    iSplitR.
+    2: { iFrame. iApply ("IH" with "[//] [$] [$]"). }
+    
+    iRight. iPureIntro. simpl. 
+    exists e2, K, s. split; [done| ].
+    eapply inside_call_extend; eauto.
+    Unshelve. all: exact tt.
+  Admitted.
+
+  Lemma start_call extr tp1 tp2 K σ1 (a: val)
+    (Hζ : locale_of tp1 (fill K (m a)) = τ)
+  (Hextr : trace_ends_in extr (tp1 ++ fill K (m a) :: tp2, σ1))
+  (e2 : expr)
+  (σ2 : state)
+  (V2 : to_val e2 = None):
+  inside_call m τ K (trace_length extr - 1)
+    (extr :tr[ Some τ ]: (tp1 ++ fill K e2 :: tp2 ++ [], σ2)) e2.
+  Proof using.
+    pose proof (trace_length_at_least extr) as LENnz. simpl in LENnz. 
+    red. simpl. exists a.
+    remember (trace_length extr) as N.
+    rewrite -HeqN. rewrite -HeqN in LENnz. 
+    repeat split.
+    - eexists.
+      split.
+      + eapply ft_lookup_old.
+        eapply trace_lookup_last. rewrite -HeqN. lia.
+      + rewrite Hextr. simpl.
+        rewrite -Hζ. rewrite /from_locale. 
+        by rewrite from_locale_from_locale_of.
+    - lia.
+    - intros j DOM.
+      assert (j = trace_length extr - 1 \/ j = trace_length extr) as [-> | ->].
+      { rewrite -HeqN. lia. }
+      + rewrite trace_lookup_extend_lt.
+        2: { rewrite -HeqN; lia. }
+        erewrite trace_lookup_last; [| rewrite -HeqN; lia]. simpl.
+        rewrite Hextr. 
+        eapply (@call_nval_at _ _ App); [done| ].
+        red. red.
+        rewrite -Hζ. rewrite /from_locale. 
+        by rewrite from_locale_from_locale_of.
+      + rewrite @trace_lookup_last.
+        2: done.
+        simpl. 
+        red. eexists. split; [| apply V2].
+        red. rewrite -Hζ. rewrite /from_locale. 
+        by rewrite from_locale_from_locale_of.
+    - rewrite app_nil_r.
+      rewrite -Hζ. rewrite /locale_of.
+      rewrite list_lookup_middle; [| done].
+      simpl. by apply under_ctx_spec.
+  Qed.
+
   (* Lemma lift_spec `{Hinv : @IEMGS heap_lang M LG EM Σ} (a: val): *)
   (*   (let _ := IEMGS_into_Looping (@pwt_Hinv _ PWT) si_add_none in *)
   (*    {{{ unit_tok }}} App m a @ τ {{{ v, RET v; unit_tok}}} ) ⊢ *)
@@ -100,41 +254,12 @@ Section SpecLifting.
     iModIntro. do 2 iExists _. iFrame.
     assert (efs = []) as -> by admit. repeat rewrite big_sepL_nil.
     iSplitR.
-    { iRight. iPureIntro. simpl. 
-      exists e2, K, (trace_length extr - 1). split; [done| ].
-      pose proof (trace_length_at_least extr) as LENnz. simpl in LENnz. 
-      red. simpl. exists a. repeat split.
-      - eexists.
-        split.
-        + eapply ft_lookup_old.
-          eapply trace_lookup_last. simpl in *. lia.
-        + rewrite Hextr. simpl.
-          rewrite -Hζ. rewrite /from_locale. 
-          by rewrite from_locale_from_locale_of.
-      - lia.
-      - intros j DOM.
-        assert (j = trace_length extr - 1 \/ j = trace_length extr) as [-> | ->].
-        { simpl. lia. }
-        + rewrite trace_lookup_extend_lt.
-          2: simpl; lia.
-          erewrite trace_lookup_last; [| simpl; lia]. simpl.
-          rewrite Hextr. 
-          eapply (@call_nval_at _ _ App); [done| ].
-          red. red.
-          rewrite -Hζ. rewrite /from_locale. 
-          by rewrite from_locale_from_locale_of.
-        + rewrite @trace_lookup_last.
-          2: done.
-          simpl. 
-          red. eexists. split; [| apply V2].
-          red. rewrite -Hζ. rewrite /from_locale. 
-          by rewrite from_locale_from_locale_of.
-      - rewrite app_nil_r.
-        rewrite -Hζ. rewrite /locale_of.
-        rewrite list_lookup_middle; [| done].
-        simpl. by apply under_ctx_spec. }
-
-    iFrame. iClear "ADD". 
-
+    2: { iFrame. by iApply (lift_call with "[$] [$]"). }  
+    
+    iRight. iPureIntro. simpl. 
+    exists e2, K, (trace_length extr - 1). split; [done| ].
+    eapply start_call; eauto. 
+    Unshelve. all: exact tt.
+  Qed.
 
 End SpecLifting. 
