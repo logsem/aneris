07793a6cf1e44eb0a6da5a7d26de8e7d65788644
fresheed <ok.john.rus@gmail.com>
2025-12-08 17:54:17 +0100

    restored adequacy proofs for simple examples

--- lawyer/nonblocking/examples/counter_adequacy.v
+++ lawyer/nonblocking/examples/counter_adequacy.v
@@ -5,8 +5,8 @@ Theorem counter_is_wait_free
   (l0: loc := Loc 0)
   : wait_free (incr l0) (counter_is_init_st l0).
 Proof using.
-  pose proof (wfree_is_wait_free _ (counter_WF_spec l0)).
-  done.
+  eapply (wfree_is_wait_free _ (counter_WF_spec l0)).
+  eauto. 
 Qed.
 
 (* Print Assumptions counter_is_wait_free.  *)
--- lawyer/nonblocking/examples/mk_ref_adequacy.v
+++ lawyer/nonblocking/examples/mk_ref_adequacy.v
@@ -2,8 +2,8 @@ From lawyer.nonblocking Require Import mk_ref wfree_adequacy wfree_traces.
 
 Theorem mk_ref_is_wait_free: wait_free mk_ref (fun _ => True).
 Proof using.
-  pose proof (wfree_is_wait_free _ mk_ref_WF_spec).
-  simpl in H. apply H. 
+  eapply (wfree_is_wait_free _ mk_ref_WF_spec).
+  eauto. 
 Qed.
 
 Print Assumptions mk_ref_is_wait_free.
