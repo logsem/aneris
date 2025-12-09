31d0b520c473b4dfcb7a34b590a86b51467e6401
fresheed <ok.john.rus@gmail.com>
2025-12-09 13:24:44 +0100

    updated simple examples' adequacy

--- lawyer/nonblocking/examples/counter_adequacy.v
+++ lawyer/nonblocking/examples/counter_adequacy.v
@@ -5,7 +5,7 @@ Theorem counter_is_wait_free
   (l0: loc := Loc 0)
   : wait_free (incr l0) (counter_is_init_st l0) NotStuck.
 Proof using.
-  eapply (wfree_is_wait_free _ (counter_WF_spec l0)).
+  eapply (wfree_is_wait_free _ _ (counter_WF_spec l0)).
   eauto. 
 Qed.
 
--- lawyer/nonblocking/examples/mk_ref_adequacy.v
+++ lawyer/nonblocking/examples/mk_ref_adequacy.v
@@ -3,7 +3,7 @@ From lawyer.nonblocking Require Import mk_ref wfree_adequacy wfree_traces.
 
 Theorem mk_ref_is_wait_free: wait_free mk_ref (fun _ => True) NotStuck.
 Proof using.
-  eapply (wfree_is_wait_free _ mk_ref_WF_spec).
+  eapply (wfree_is_wait_free _ _ mk_ref_WF_spec).
   eauto. 
 Qed.
 
