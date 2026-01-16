599f1d9f6f9dc72bc07c6b164097805e6d281cc2
fresheed <ok.john.rus@gmail.com>
2026-01-16 17:57:56 +0100

    wip

--- lawyer/nonblocking/examples/queue/other_methods.v
+++ lawyer/nonblocking/examples/queue/other_methods.v
@@ -266,14 +266,160 @@ Section OtherMethods.
     iApply "POST". iFrame.    
   Qed.
 
+  Definition is_LL_into_alt (hq: HistQueue) (pt: loc) := 
+    (forall (i: nat) (nd nd': HistNode),
+        hq !! i = Some nd -> hq !! (S i) = Some nd' -> nd.2.2 = nd'.1) /\
+    from_option (fun nd => nd.2.2 = pt) True (last hq).
+
+  Lemma is_LL_into_equiv hq pt:
+    is_LL_into hq pt <-> is_LL_into_alt hq pt.
+  Proof using.
+    induction hq.
+    { done. }
+    destruct a as [? [??]]. simpl.
+    destruct hq.
+    { split.
+      - intros ->. red. set_solver.
+      - intros [??]. done. }
+    destruct h as [? [??]].
+    split.
+    - intros [-> LL].
+      apply IHhq in LL as [NEXT FIN]. clear IHhq.
+      red. split.
+      2: done.
+      simpl.
+      intros. destruct i.
+      { simpl in *. set_solver. }
+      simpl in *.
+      ospecialize * (NEXT i _ _); eauto.
+    - intros LL.
+      destruct LL as [NEXT FIN].      
+      apply proj2 in IHhq. ospecialize * IHhq.
+      { red. split; [| done].
+        intros. eapply (NEXT (S i)); eauto. }
+      split; [| done].
+      ospecialize (NEXT 0 _ _ _ _ ); eauto.
+  Qed.
+  
+  Lemma is_LL_into_ext l p1 v p2
+    (LL: is_LL_into l p1):
+  is_LL_into (l ++ [(p1, (v, p2))]) p2.
+  Proof using.
+    apply is_LL_into_equiv. apply is_LL_into_equiv in LL.
+    red. destruct LL as [NEXT FIN]. split.
+    2: by rewrite last_snoc. 
+    intros.
+    pose proof H0 as LEN%lookup_lt_Some. rewrite length_app /= in LEN.
+    assert (i < length l) as LEN' by lia. clear LEN.
+    red in LEN'. apply Nat.le_lteq in LEN' as [LEN | EQ].
+    - rewrite lookup_app_l in H; eauto.
+      rewrite lookup_app_l in H0; eauto. lia.
+    - rewrite EQ in H0.
+      rewrite list_lookup_middle in H0; [| done].
+      inversion H0. subst nd'. simpl.
+      rewrite last_lookup in FIN. rewrite -EQ /= in FIN.
+      rewrite lookup_app_l in H; eauto.
+      2: lia.
+      by rewrite H in FIN.
+  Qed.    
+
+  Lemma hq_auth_extend hq nd:
+    hq_auth hq ==∗ hq_auth (hq ++ [nd]).
+  Proof using.
+    rewrite /hq_auth.
+    iIntros "[AUTH FRAG]".
+    rewrite big_sepL_app. iFrame "FRAG".
+    rewrite big_sepL_singleton. rewrite Nat.add_0_r.
+
+    assert (length hq ∉ dom $ list_to_imap hq).
+    { rewrite list_to_imap_dom. rewrite elem_of_set_seq. lia. }
+
+    replace (to_agree <$> list_to_imap (hq ++ [nd])) with (<[ length hq := to_agree nd ]> (to_agree <$> list_to_imap hq)).
+    2: { rewrite -fmap_insert. f_equal.
+         rewrite /list_to_imap. simpl.
+         rewrite imap_app. simpl.
+         rewrite Nat.add_0_r. rewrite list_to_map_app /=.
+         rewrite insert_empty. rewrite map_union_comm.
+         2: { apply map_disjoint_dom. set_solver. }
+         by rewrite -insert_union_singleton_l. }
+
+    rewrite /ith_node. rewrite -own_op.
+    iApply (own_update with "[$]").
+    apply auth_update_alloc.
+    apply alloc_singleton_local_update; [| done].
+    apply not_elem_of_dom. by rewrite dom_fmap.
+  Qed.
+
   Lemma update_tail l (τ: locale heap_lang) (π: Phase) (q: Qp)
-    pn pt nd    t br:
-    {{{ queue_inv l ∗ hn_interp (pn, dummy_node) ∗ hn_interp_wip (pt, nd) ∗ 
+    pn pt v    t br:
+    {{{ queue_inv l ∗ hn_interp (pn, dummy_node) ∗ hn_interp_wip (pt, (v, pn)) ∗ 
         read_head_resources t br pt None ∗
         th_phase_frag τ π q ∗ cp_mul π d 1 }}}
       #(Tail q_sq) <- #pn @τ
     {{{ RET #(); th_phase_frag τ π q ∗ read_head_resources (S t) br pn None ∗ rop_token }}}.
-  Proof using. Admitted. 
+  Proof using.
+    simpl. iIntros (Φ) "([#QAT #INV] & DNI & TNI & RH & PH & CPS) POST".
+    destruct q_sq eqn:Q_SQ. simpl.
+
+    iInv "INV" as "(%hq & %h & %t_ & %br_ & %fl & %rop_ & %od & %hist & inv)" "CLOS".
+    iEval (rewrite /queue_inv_inner) in "inv".
+    iDestruct "inv" as ">(HQ & AUTHS & %ORDER0 & QI & DANGLE & OHV & RHI & RH' & DQ)".
+    iDestruct (read_head_resources_auth_agree with "RH [$]") as %[<- <-].
+    iDestruct (read_head_resources_rop_interp_agree with "RH [$]") as %<-.
+
+    rewrite /queue_interp. iDestruct "QI" as "(%T_LEN & PQI & BR & FL)".
+    rewrite {1}/phys_queue_interp. iDestruct "PQI" as "(Q & (%pt_ & TAIL & DUMMY & %LL & HEAD))".
+    iDestruct (read_head_res_Tail_agree with "RH [$]") as %EQ.
+    inversion EQ. subst pt_.
+
+    rewrite {1}/read_head_resources. iDestruct "RH" as "(TE & BRE & TAIL' & ROP)".
+    iCombine "TAIL TAIL'" as "TAIL".
+    rewrite Q_SQ /=.
+
+    iApply sswp_MU_wp; [done| ].
+    iApply (wp_store with "[$]"). iIntros "!> [TAIL TAIL']".
+    MU_by_burn_cp. iApply wp_value.
+
+    iAssert (|==> read_head_resources (S t) br pn None ∗ auths h (S t) br fl)%I with "[TE BRE TAIL' ROP AUTHS]" as "X".
+    { iFrame "BRE ROP".
+      rewrite Q_SQ /=. iFrame.
+      rewrite /auths. iDestruct "AUTHS" as "($ & TA & $ & $)".
+      rewrite bi.sep_comm. iApply (me_update with "TA [$]"). lia. }
+    iMod "X" as "[RH AUTHS]". 
+    
+    iAssert (phys_queue_interp (drop h (hq ++ [(pt, (v, pn))])) ∗ rop_token)%I
+      with "[Q HEAD TAIL DNI TNI DUMMY]" as "[PQI ROP]".
+    { rewrite /phys_queue_interp.
+      rewrite skipn_app.    
+      rewrite (proj2 (Nat.sub_0_le _ _)).
+      2: { red in ORDER0. lia. }
+      simpl. rewrite big_sepL_app big_sepL_singleton. iFrame "Q".
+
+      iDestruct "TNI" as "[TNI0 TNI1]".
+      iDestruct "DUMMY" as "[[TNI0' ?] | (% & TNI0' & RTOK')]".
+      { by iDestruct (pointsto_ne with "TNI1 [$]") as %?. }
+      rewrite -bi.sep_assoc. iSplitL "TNI0 TNI0' TNI1".
+      { iCombine "TNI0 TNI0'" as "X".
+        rewrite frac_op Qp.half_half. iFrame. }
+
+      rewrite Q_SQ /=. iFrame.
+      iSplitR.
+      { iPureIntro. eapply is_LL_into_ext; eauto. }
+      remember (drop h hq) as hq'.
+      destruct hq'; done. }
+
+    iAssert (queue_interp (hq ++ [(pt, (v, pn))]) h (S t) br fl)%I 
+      with "[PQI BR FL]" as "QI".
+    { iFrame. iSplitR.
+      { iPureIntro. rewrite length_app /=. lia. }
+      rewrite Q_SQ /=. 
+      iDestruct "BR" as "(% & %BR & BR)". iDestruct "FL" as "(% & %FL & FR & ?)".
+      iFrame. iPureIntro.
+      split; eapply lookup_app_l_Some; eauto. }
+
+
+    
+  Admitted. 
 
   Lemma enqueue_spec l (τ: locale heap_lang) (π: Phase) (q: Qp) (v: val):
     {{{ queue_inv l ∗ read_head_token ∗ 
