cba58d1aeb1d1e6ed2de02dedf0c697b38410907
fresheed <ok.john.rus@gmail.com>
2026-01-27 13:43:53 +0100

    implemented tokens RA

--- lawyer/nonblocking/tokens/tokens_ra.v
+++ lawyer/nonblocking/tokens/tokens_ra.v
@@ -1,37 +1,47 @@
 From iris.proofmode Require Import tactics.
-From iris.algebra Require Import gmultiset.
+From iris.algebra Require Import gmultiset auth.
 From heap_lang Require Import lang.
 
 
 Class MethodTokenPre (Σ: gFunctors) := {
-    (* mt_pre_tok :: inG Σ (exclR positive); *)
+    mt_pre_tok :: inG Σ (authR (gmultiset val));
 }.
 Class MethodToken (MS: gmultiset val) (Σ: gFunctors) := {
-    (* ut_pre_pre :: MethodTokenPre Σ; *)
+    mt_pre :: MethodTokenPre Σ;
     mt_γ: gname;
 }.
 
+Definition mt_Σ: gFunctors := #[GFunctor $ authR (gmultiset val)].
+Global Instance mt_sub: forall Σ, subG mt_Σ Σ -> MethodTokenPre Σ.
+Proof using. solve_inG. Qed.
 
-(* TODO: generalize, upstream *)
+
+(* TODO: generalize, upstream? *)
 Section TokensRA.
   Context `{MethodToken MS Σ}.
   
-  Definition methods_toks (ms: gmultiset val): iProp Σ. Admitted.
-
-  (* Global Instance is_op_mt m1 m2 :  *)
-  (*   IsOp (methods_toks (m1 ⊎ m2)) (methods_toks m1) (methods_toks m2). *)
-  (* Proof. by rewrite /IsOp' /IsOp frac_op Qp.div_2. Qed. *)
-
-  Global Instance methods_toks_tl ms: Timeless (methods_toks ms).
-  Proof using. Admitted.
+  Definition methods_toks (ms: gmultiset val): iProp Σ :=
+    own mt_γ (●{DfracDiscarded} MS ⋅ ◯ ms).    
 
   Lemma methods_toks_split ms1 ms2:
     methods_toks (ms1 ⊎ ms2) ⊣⊢ methods_toks ms1 ∗ methods_toks ms2.
-  Proof using. Admitted.
+  Proof using.
+    rewrite /methods_toks. rewrite -own_op.
+    apply own_proper.
+    rewrite -gmultiset_op. rewrite auth_frag_op.
+    rewrite !cmra_assoc. apply cmra_op_proper'; [| done].
+    rewrite cmra_comm. rewrite -cmra_assoc. apply cmra_op_proper'; [done| ].
+    rewrite -auth_auth_dfrac_op. by rewrite dfrac_op_discarded.
+  Qed.
 
   Lemma methods_toks_sub ms:
     methods_toks ms -∗ ⌜ ms ⊆ MS ⌝.
-  Proof using. Admitted. 
+  Proof using.
+    rewrite /methods_toks. iIntros "X".
+    iDestruct (own_valid with "X") as %V. iPureIntro.
+    apply auth_both_dfrac_valid_discrete in V.
+    rewrite gmultiset_included in V. apply V.
+  Qed.
 
   Definition method_tok (m: val) := methods_toks {[+ m +]}. 
 
@@ -45,5 +55,10 @@ Section TokensRA.
 End TokensRA.
 
 Lemma mt_init `{MethodTokenPre Σ} MS:
-  ⊢ ∃ (MT: MethodToken MS Σ), methods_toks MS.
-Proof using. Admitted.
+  ⊢ |==> ∃ (MT: MethodToken MS Σ), methods_toks MS.
+Proof using.
+  iStartProof. 
+  iMod (own_alloc (●{DfracDiscarded} MS ⋅ ◯ MS)) as (γ) "X".
+  { apply auth_both_dfrac_valid_2; done. }
+  iExists {| mt_γ := γ |}. by iFrame.
+Qed.
